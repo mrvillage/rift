0343abd37f27c4c64f452364589d375980f5f460
Village05 <mr.village05@gmail.com>
2021-05-07 12:52:27 -0400

    Made progress on updating formatting and removing connection arguments

--- source/data/get/link.py
+++ source/data/get/link.py
@@ -7,7 +7,7 @@ async def get_links(connection):
     """)
 
 
-async def add_link(connection, user_id, nation_id):
+async def add_link(user_id, nation_id):
     return await execute_query(f"""
         INSERT INTO links
         VALUES
@@ -15,28 +15,28 @@ async def add_link(connection, user_id, nation_id):
     """)
 
 
-async def remove_link_user(connection, user_id):
+async def remove_link_user(user_id):
     return await execute_query(f"""
         DELETE FROM links
         WHERE id = {user_id};
     """)
 
 
-async def remove_link_nation(connection, nation_id):
+async def remove_link_nation(nation_id):
     return await execute_query(f"""
         DELETE FROM links
         WHERE nation = {nation_id};
     """)
 
 
-async def get_link_user(connection, user_id):
+async def get_link_user(user_id):
     return (await execute_read_query(f"""
         SELECT * FROM links
         WHERE id = {user_id};
     """))[0]
 
 
-async def get_link_nation(connection, nation_id):
+async def get_link_nation(nation_id):
     return (await execute_read_query(f"""
         SELECT * FROM links
         WHERE nation = {nation_id};
