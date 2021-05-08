07fe19cb9469d79c0e8f370457ea2fe7bae65c1c
Village05 <mr.village05@gmail.com>
2021-05-08 01:06:08 -0400

    Improved nation and alliance searching

--- source/funcs/nation.py
+++ source/funcs/nation.py
@@ -1,1 +1,27 @@
-pass
+from discord.ext.commands import Context, MemberConverter, UserConverter, MemberNotFound, UserNotFound
+from .link import get_link_user, get_link_nation
+from .. import cache
+from ..errors import NationNotFoundError
+from ..data import get
+
+
+async def search_nation(ctx: Context, search):
+    try:
+        user = await MemberConverter().convert(ctx, search)
+    except MemberNotFound:
+        try:
+            user = await UserConverter().convert(ctx, search)
+        except UserNotFound:
+            pass
+    if "user" in locals():
+        return cache.nations[(await get_link_user(user.id))[1]]
+    if search.isdigit():
+        try:
+            return cache.nations[int(search)]
+        except KeyError:
+            pass
+    nation = await get.get_nation(search)
+    if nation is not None:
+        return nation
+    nation = await get.get_nation(search)
+    raise NationNotFoundError
