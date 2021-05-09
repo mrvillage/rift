a1cd557a0f893fb3918672f23ac18a8329348337
Village05 <mr.village05@gmail.com>
2021-05-08 22:57:02 -0400

    Updated to use search package and have PnW links

--- source/jobs/target.py
+++ source/jobs/target.py
@@ -26,18 +26,22 @@ async def target_check():
                             mentions = ""
                         if len(json.loads(target[6])) != 0:
                             mentions += f"<@{f'><@'.join([str(i) for i in json.loads(target[6])])}>"
-                        embed = rift.get_embed_author_member(channel.guild, f"[Nation Page](https://politicsandwar.com/nation/id={nation.id} \"https://politicsandwar.com/nation/id={nation.id}\")", timestamp=bot.nations_update, footer="Data collected at", fields=[
+                        embed = rift.get_embed_author_guild(channel.guild, f"[Nation Page](https://politicsandwar.com/nation/id={nation.id} \"https://politicsandwar.com/nation/id={nation.id}\")", timestamp=bot.nations_update, footer="Data collected at", fields=[
                             {"name": "Nation ID", "value": nation.id},
                             {"name": "Nation Name", "value": nation.name},
                             {"name": "Leader Name", "value": nation.leader},
                             {"name": "War Policy", "value": nation.war_policy},
-                            {"name": "Domestic Policy", "value": nation.domestic_policy},
+                            {"name": "Domestic Policy",
+                                "value": nation.domestic_policy},
                             {"name": "Continent", "value": nation.continent},
                             {"name": "Color", "value": nation.color if nation.color !=
                                 "Beige" else f"Beige ({nation.beige_turns:,} Turns"},
-                            {"name": "Alliance", "value": f"[{repr(nation.alliance)}](https://politicsandwar.com/alliance/id={nation.alliance.id} \"https://politicsandwar.com/alliance/id={nation.alliance.id}\")"},
-                            {"name": "Alliance Position", "value": nation.alliance_position},
-                            {"name": "Cities", "value": nation.cities},
+                            {"name": "Alliance",
+                                "value": f"[{repr(nation.alliance)}](https://politicsandwar.com/alliance/id={nation.alliance.id} \"https://politicsandwar.com/alliance/id={nation.alliance.id}\")" if nation.alliance is not None else "None"},
+                            {"name": "Alliance Position",
+                                "value": nation.alliance_position},
+                            {"name": "Cities",
+                                "value": f"[{nation.cities}](https://politicsandwar.com/?id=62&n={'+'.join(nation.name.split(' '))} \"https://politicsandwar.com/?id=62&n={'+'.join(nation.name.split(' '))}\")"},
                             {"name": "Score", "value": f"{nation.score:,.2f}"},
                             {"name": "Vacation Mode",
                                 "value": f"True ({nation.v_mode_turns:,} Turns)" if nation.v_mode else "False"},
@@ -47,9 +51,19 @@ async def target_check():
                             {"name": "Ships", "value": f"{nation.ships:,}"},
                             {"name": "Missiles", "value": f"{nation.missiles:,}"},
                             {"name": "Nukes", "value": f"{nation.nukes:,}"},
-                            {"name": "Offensive Wars", "value": nation.offensive_wars},
-                            {"name": "Defensive Wars", "value": nation.defensive_wars},
-                            {"name": "Average Infrastructure", "value": f"{nation.avg_infra():,.2f}"},
+                            {"name": "Offensive Wars",
+                                "value": f"[{nation.offensive_wars}](https://politicsandwar.com/nation/id={nation.id}&display=war \"https://politicsandwar.com/nation/id={nation.id}&display=war\")"},
+                            {"name": "Defensive Wars",
+                                "value": f"[{nation.defensive_wars}](https://politicsandwar.com/nation/id={nation.id}&display=war \"https://politicsandwar.com/nation/id={nation.id}&display=war\")"},
+                            {"name": "Average Infrastructure",
+                                "value": f"{nation.avg_infra():,.2f}"},
+                            {"name": "Actions", "value":
+                                f"[Message](https://politicsandwar.com/inbox/message/receiver={'+'.join(nation.leader.split(' '))} \"https://politicsandwar.com/inbox/message/receiver={'+'.join(nation.leader.split(' '))}\") "
+                                f"[Trade](https://politicsandwar.com/nation/trade/create/nation={'+'.join(nation.name.split(' '))} \"https://politicsandwar.com/nation/trade/create/nation={'+'.join(nation.name.split(' '))}\") "
+                                f"[Embargo](https://politicsandwar.com/index.php?id=68&name={'+'.join(nation.name.split(' '))}&type=n \"https://politicsandwar.com/index.php?id=68&name={'+'.join(nation.name.split(' '))}&type=n\") "
+                                f"[War](https://politicsandwar.com/nation/war/declare/id={nation.id} \"https://politicsandwar.com/nation/war/declare/id={nation.id}\") "
+                                f"[Espionage](https://politicsandwar.com/nation/espionage/eid={nation.id} \"https://politicsandwar.com/nation/espionage/eid={nation.id}\") "
+                             }
                         ])
                         await channel.send(mentions, embed=embed)
             await rift.update_target_color(nation.id, rift.get_color_id(nation.color))
