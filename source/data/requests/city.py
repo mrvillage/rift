7514a9f8f4610e86cfd00c1d840e707c477fd63d
Village05 <mr.village05@gmail.com>
2021-05-11 17:12:59 -0400

    Moved to aiohttp.request

--- source/data/requests/city.py
+++ source/data/requests/city.py
@@ -1,6 +1,7 @@
+import aiohttp
 from ...ref import bot
 
 
 async def get_city_build(*, city_id):
-    async with bot.pnw_session.get(f"https://politicsandwar.com/api/city_export.php?city_id={city_id}") as response:
+    async with aiohttp.request("GET", f"https://politicsandwar.com/api/city_export.php?city_id={city_id}") as response:
         return await response.json()
