709599958d694409309aece3ca274b7b36f394fa
Village05 <mr.village05@gmail.com>
2021-05-27 12:37:04 -0400

    Updated formatting

--- source/bot/cogs/link.py
+++ source/bot/cogs/link.py
@@ -1,4 +1,4 @@
-
+from typing import Union
 import discord
 from discord.ext import commands
 from ... import funcs as rift
@@ -9,27 +9,38 @@ class Link(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="link", aliases=["verify"], help="Links your Politics and War nation to your Discord account.")
+    @commands.command(name="link", aliases=["verify"],
+                      help="Links your Politics and War nation to your Discord account.")
     async def link(self, ctx, nation, user: Union[discord.Member, discord.User] = None):
         try:
             nation = await rift.search_nation(ctx, nation)
         except NationNotFoundError:
-            await ctx.reply(embed=rift.get_embed_author_member(ctx.author, f"No nation found with argument `{nation}`."))
+            await ctx.reply(embed=rift.get_embed_author_member(
+                ctx.author,
+                f"No nation found with argument `{nation}`."))
             return
         user = ctx.author if user is None else user
         links = await rift.get_links()
         if any(user.id in tuple(i) for i in links):
-            await ctx.reply(embed=rift.get_embed_author_member(user, f"{user.mention} is already linked!"))
+            await ctx.reply(embed=rift.get_embed_author_member(
+                user,
+                f"{user.mention} is already linked!"))
             return
         if any(user.id in tuple(i) for i in links):
-            await ctx.reply(embed=rift.get_embed_author_member(user, f"Nation `{nation.id}` is already linked!"))
+            await ctx.reply(embed=rift.get_embed_author_member(
+                user,
+                f"Nation `{nation.id}` is already linked!"))
             return
         try:
-            message = await ctx.reply(embed=rift.get_embed_author_member( user, "Fetching Discord username..."))
+            message = await ctx.reply(embed=rift.get_embed_author_member(
+                user,
+                "Fetching Discord username..."))
             name = await nation.get_discord_page_username()
             if name == f"{user.name}#{user.discriminator}":
                 await rift.add_link(user.id, nation.id)
-                await message.edit(embed=rift.get_embed_author_member(user, f"Success! {user.mention} is now linked to nation `{nation.id}`!"))
+                await message.edit(embed=rift.get_embed_author_member(
+                    user,
+                    f"Success! {user.mention} is now linked to nation `{nation.id}`!"))
             else:
                 await message.edit(embed=rift.get_embed_author_member(
                     user,
