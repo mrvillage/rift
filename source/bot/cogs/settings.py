afe6c2d76dca39ba63cce21991a50e3d1736317d
Village05 <mr.village05@gmail.com>
2021-06-28 18:33:30 -0400

    Events and settings cogs creation

---
+++ source/bot/cogs/settings.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from ... import funcs as rift
+
+
+class Settings(commands.Cog):
+    def __init__(self, bot: rift.Rift):
+        self.bot = bot
+
+    @commands.group(
+        name="user-settings", aliases=["usersettings"], invoke_without_command=True
+    )
+    async def user_settings(self, ctx):
+        pass
+
+    @commands.group(
+        name="server-settings", aliases=["serversettings"], invoke_without_command=True
+    )
+    async def server_settings(self, ctx):
+        pass
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Settings(bot))
