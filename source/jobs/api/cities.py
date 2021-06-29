dca8ca759ed1162e490f781a2dd681c6a87bdbf6
Village05 <mr.village05@gmail.com>
2021-06-28 21:15:15 -0400

    Black file formatting

--- source/jobs/api/cities.py
+++ source/jobs/api/cities.py
@@ -9,7 +9,8 @@ from ...data import cache
 
 async def fetch_cities():
     try:
-        await execute_query("""
+        await execute_query(
+            """
             CREATE TABLE "citiesnew" (
                 "id"	INTEGER NOT NULL UNIQUE,
                 "nation_id" INTEGER,
@@ -20,10 +21,12 @@ async def fetch_cities():
                 "land" REAL,
                 PRIMARY KEY("id")
             );
-            """)
+            """
+        )
     except DuplicateTableError:
         await execute_query("DROP TABLE citiesnew;")
-        await execute_query("""
+        await execute_query(
+            """
             CREATE TABLE "citiesnew" (
                 "id"	INTEGER NOT NULL UNIQUE,
                 "nation_id" INTEGER,
@@ -34,25 +37,36 @@ async def fetch_cities():
                 "land" REAL,
                 PRIMARY KEY("id")
             );
-            """)
+            """
+        )
     try:
-        async with aiohttp.request("GET", f"{BASEURL}/all-cities/key={APIKEY}") as response:
+        async with aiohttp.request(
+            "GET", f"{BASEURL}/all-cities/key={APIKEY}"
+        ) as response:
             collected = datetime.datetime.utcnow()
             cities = await response.json()
-            cities_ = [(
-                int(i['city_id']),
-                int(i['nation_id']),
-                i['city_name'],
-                bool(int(i['capital'])),
-                float(i['infrastructure']),
-                float(i['maxinfra']),
-                float(i['land'])
-            ) for i in cities['all_cities']]
-            await execute_query_many("INSERT INTO citiesnew (id, nation_id, city_name, capital, infrastructure, maxinfra, land) VALUES ($1, $2, $3, $4, $5, $6, $7);", cities_)  # pylint: disable=line-too-long
-        await execute_query("""
+            cities_ = [
+                (
+                    int(i["city_id"]),
+                    int(i["nation_id"]),
+                    i["city_name"],
+                    bool(int(i["capital"])),
+                    float(i["infrastructure"]),
+                    float(i["maxinfra"]),
+                    float(i["land"]),
+                )
+                for i in cities["all_cities"]
+            ]
+            await execute_query_many(
+                "INSERT INTO citiesnew (id, nation_id, city_name, capital, infrastructure, maxinfra, land) VALUES ($1, $2, $3, $4, $5, $6, $7);",
+                cities_,
+            )  # pylint: disable=line-too-long
+        await execute_query(
+            """
             DROP TABLE cities;
             ALTER TABLE citiesnew RENAME TO cities;
-        """)
+        """
+        )
         bot.cities_update = collected
     except Exception as error:  # pylint: disable=broad-except
         print("FATAL ERROR RETRIEVING CITY DATA", error)
