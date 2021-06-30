5708de1221dbb8a55a5600e00a3678d9c81991da
Village05 <mr.village05@gmail.com>
2021-06-30 00:39:59 -0400

    FIxed make_attrs in Base

--- source/data/classes/base.py
+++ source/data/classes/base.py
@@ -1,4 +1,4 @@
 class Base:
     async def make_attrs(self, *attrs):
         for attr in attrs:
-            await getattr(self, f"_make-{attr}")
+            await getattr(self, f"_make_{attr}")()
