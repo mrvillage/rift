a7cef900513c4f98133b8bbedf880942ad52e983
Village05 <mr.village05@gmail.com>
2021-07-08 23:26:22 -0400

    Flag experiments

--- src/bot/cogs/experiments.py
+++ src/bot/cogs/experiments.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from discord.ext import commands
 
 from ...data.query import get_mmr
@@ -10,6 +11,17 @@ class Experiments(commands.Cog, command_attrs=dict(hidden=True)):
     @commands.command(name="mmr")
     async def mmr(self, ctx):
         print(await get_mmr(mmr_id=1))
+
+    class Flags(
+        commands.FlagConverter, case_insensitive=True, delimiter="=", prefix="-"
+    ):
+        hi: Optional[str] = commands.flag(name="hi", default=lambda ctx: [])
+        hello: Optional[str] = commands.flag(name="hello", default=lambda ctx: [])
+
+    @commands.command(name="flags")
+    async def flags(self, ctx, flags: Flags, *, kwarg=None):
+        await ctx.send(str(kwarg))
+        await ctx.send(str(flags))
 
 
 def setup(bot):
