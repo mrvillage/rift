4da13dc23c9b8a0b684b238166b382b1928cc3fd
Village05 <mr.village05@gmail.com>
2021-07-08 23:41:34 -0400

    Fixed SQL queries

--- src/data/query/nation.py
+++ src/data/query/nation.py
@@ -7,8 +7,9 @@ async def get_nation(*, nation_id=None, nation_name=None):
             await execute_read_query(
                 f"""
             SELECT * FROM nations
-            WHERE nation_id = {nation_id};
-        """
+            WHERE nation_id = $1;
+        """,
+                int(nation_id),
             )
         )[0]
     elif nation_name is not None:
@@ -16,8 +17,9 @@ async def get_nation(*, nation_id=None, nation_name=None):
             await execute_read_query(
                 f"""
             SELECT * FROM nations
-            WHERE LOWER(nation) = LOWER({nation_name});
-        """
+            WHERE LOWER(nation) = LOWER($1);
+        """,
+                str(nation_name),
             )
         )[0]
 
