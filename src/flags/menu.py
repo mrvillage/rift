e13ab7042927a20bb5737890f9a4395a704fc746
Village05 <mr.village05@gmail.com>
2021-07-12 12:03:13 -0400

    Updated menu flags

--- src/flags/menu.py
+++ src/flags/menu.py
@@ -1,33 +1,34 @@
-from typing import Tuple, Union
+from typing import Optional, Tuple, Union
 
 from discord import PartialEmoji
 from discord.emoji import Emoji
+from discord.ext.commands.flags import flag
 
 from .base import BaseFlagConverter
 
 
 class ButtonFlags(BaseFlagConverter):
-    action: str
-    url: str
-    disabled: bool
-    style: str
-    color: str
-    label: str
-    name: str
-    emoji: Union[PartialEmoji, Emoji]
+    action: Optional[str]
+    url: Optional[str]
+    disabled: Optional[bool] = flag(default=False)
+    style: Optional[str] = flag(aliases=["color"])
+    label: str = flag(aliases=["name"])
+    emoji: Optional[Union[PartialEmoji, Emoji]]
     options: Tuple[str, ...]
+    row: Optional[int]
 
 
 class SelectFlags(BaseFlagConverter):
-    placeholder: str
-    min_values: int
-    max_values: int
+    placeholder: Optional[str]
+    min_values: Optional[int]
+    max_values: Optional[int]
 
 
 class SelectOptionFlags(BaseFlagConverter):
-    default: str
+    default: bool
     description: str
     emoji: Union[PartialEmoji, Emoji]
-    label: str
+    label: str = flag(name="label", aliases=["name"])
     action: str
     options: Tuple[str, ...]
+    row: Optional[int]
