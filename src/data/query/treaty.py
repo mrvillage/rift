ff1d087aa0d3a844ed42bfc8f4ef740370fa893c
Village05 <mr.village05@gmail.com>
2021-07-13 19:53:28 -0400

    Added treaties query

---
+++ src/data/query/treaty.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from typing import Sequence, Tuple, Union
+
+from ..db import execute_read_query
+
+
+async def get_treaties(alliance_id: int) -> Sequence[Tuple[Union[int, str]]]:
+    return [
+        tuple(i)
+        for i in await execute_read_query(
+            "SELECT * FROM treatiesUPDATE WHERE from_ = $1 OR to_ = $1;",
+            alliance_id,
+        )
+    ]
