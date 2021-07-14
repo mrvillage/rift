cdaec4cfbb323c7d30c29d776668330e985947d1
Village05 <mr.village05@gmail.com>
2021-07-13 21:47:52 -0400

    Updated menu queries

--- src/data/query/menu.py
+++ src/data/query/menu.py
@@ -5,9 +5,19 @@ from typing import Any, Tuple, Union
 from ..db import execute_read_query
 
 
-async def get_menu(*, menu_id: str) -> Tuple[Any]:
+async def get_menu(*, menu_id: int) -> Tuple[Any]:
     return tuple(
         (await execute_read_query("SELECT * FROM menus WHERE menu_id = $1;", menu_id))[
             0
         ]
+    )
+
+
+async def get_menu_item(*, item_id: int) -> Tuple[Any]:
+    return tuple(
+        (
+            await execute_read_query(
+                "SELECT * FROM menu_items WHERE item_id = $1;", item_id
+            )
+        )[0]
     )
