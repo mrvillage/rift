f47838360b336e534c80e4ac685d40c5fed8a7e2
Village05 <mr.village05@gmail.com>
2021-07-13 22:20:57 -0400

    Added insert interface

--- src/data/query/menu.py
+++ src/data/query/menu.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import Any, Tuple, Union
 
-from ..db import execute_read_query
+from ..db import execute_query, execute_read_query
 
 
 async def get_menu(*, menu_id: int) -> Tuple[Any]:
@@ -20,4 +20,12 @@ async def get_menu_item(*, item_id: int) -> Tuple[Any]:
                 "SELECT * FROM menu_items WHERE item_id = $1;", item_id
             )
         )[0]
+    )
+
+
+async def insert_interface(*, menu_id: int, message_id: int) -> None:
+    await execute_query(
+        "INSERT INTO menu_interfaces (menu_id, message_id) VALUES ($1, $2);",
+        menu_id,
+        message_id,
     )
