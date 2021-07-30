81bef4aad748be0dd9af4edf252868194987fca1
Village05 <mr.village05@gmail.com>
2021-07-30 18:51:04 -0400

    Update Confirm View

--- src/views/confirm.py
+++ src/views/confirm.py
@@ -1,7 +1,10 @@
+from __future__ import annotations
+
 from typing import Optional
 
-from discord import Interaction
-from .. import ui
+from discord import ButtonStyle, Interaction, ui
+
+__all__ = ("Confirm",)
 
 
 class Confirm(ui.View):
@@ -15,13 +18,13 @@ class Confirm(ui.View):
         super().__init__()
         self.value = None
 
-    @ui.button(label="Yes", style=ui.ButtonStyle.green)
+    @ui.button(label="Yes", style=ButtonStyle.green)
     async def yes(self, button: ui.Button, interaction: Interaction) -> None:
         self.interaction = interaction
         self.value = True
         self.stop()
 
-    @ui.button(label="No", style=ui.ButtonStyle.red)
+    @ui.button(label="No", style=ButtonStyle.red)
     async def no(self, button: ui.Button, interaction: Interaction) -> None:
         self.interaction = interaction
         self.value = False
