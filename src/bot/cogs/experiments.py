c0c411eaaee9c42b9c83330dba58b14c635aad76
Village05 <mr.village05@gmail.com>
2021-07-30 15:13:01 -0400

    Add some more View and Embed.provider experiments

--- src/bot/cogs/experiments.py
+++ src/bot/cogs/experiments.py
@@ -1,9 +1,13 @@
+import asyncio
 from src.flags.base import BooleanFlagConverter
 from typing import Optional
+from ... import ui
+import discord
 from discord.ext import commands
 from discord.ext.commands.flags import FlagConverter
 
 from ...data.query import get_mmr
+from ... import funcs
 
 
 class Flags(
@@ -14,6 +18,18 @@ class Flags(
 ):
     hi: Optional[str]
     hello: Optional[str]
+
+
+class Vie(discord.ui.View):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @discord.ui.select(
+        custom_id="hiahtueaht",
+        options=[discord.SelectOption(label="Hi"), discord.SelectOption(label="Hello")],
+    )
+    async def hiahtueaht(self, ctx):
+        ...
 
 
 class Experiments(commands.Cog, command_attrs=dict(hidden=True)):
@@ -28,6 +44,37 @@ class Experiments(commands.Cog, command_attrs=dict(hidden=True)):
     async def flags(self, ctx, *, flags: Flags):
         # flags = Flags.parse_flags(flags)
         await ctx.send(str(flags))
+
+    @commands.command(name="view")
+    async def view(self, ctx):
+        class Viewer(ui.View):
+            def __init__(self) -> None:
+                print("HI")
+                super().__init__()
+                print("HI")
+
+            @ui.button(label="Hello")
+            async def oogabooga(self, btn: ui.Button, interaction: discord.Interaction):
+                followup = interaction.followup
+                await interaction.response.defer()
+                await asyncio.sleep(15)
+                await followup.send(f"Hello", ephemeral=True)
+
+        view = Viewer()
+        vie = Vie()
+        await ctx.send("View", view=view)
+
+    @commands.command(name="provider")
+    async def provider(self, ctx: commands.Context):
+        embed = discord.Embed(description="This is a provider")
+        print(embed.provider)
+        embed._provider = {
+            "name": "Ash Development",
+            "url": "https://rift.mrvillage.dev",
+        }
+        print(embed.provider)
+        print(embed._provider)
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
