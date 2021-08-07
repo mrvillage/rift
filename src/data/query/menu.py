ed607d5d0a6aa35276001c12ad0d665ac7bb9dcd
Village05 <mr.village05@gmail.com>
2021-08-07 00:00:06 -0400

    Update menu queries

--- src/data/query/menu.py
+++ src/data/query/menu.py
@@ -1,22 +1,23 @@
 from __future__ import annotations
 
-from typing import Any, List, Tuple
+from typing import TYPE_CHECKING, Any, Dict, List
 
+import discord
 from discord import Message
 
 from ..db import execute_query, execute_read_query
 
 
-async def get_menu(*, menu_id: int) -> Tuple[Any]:
-    return tuple(
+async def get_menu(*, menu_id: int) -> Dict[str, Any]:
+    return dict(
         (await execute_read_query("SELECT * FROM menus WHERE menu_id = $1;", menu_id))[
             0
         ]
     )
 
 
-async def get_menu_item(*, item_id: int) -> Tuple[Any]:
-    return tuple(
+async def get_menu_item(*, item_id: int) -> Dict[str, Any]:
+    return dict(
         (
             await execute_read_query(
                 "SELECT * FROM menu_items WHERE item_id = $1;", item_id
@@ -26,22 +27,22 @@ async def get_menu_item(*, item_id: int) -> Tuple[Any]:
 
 
 async def insert_interface(*, menu_id: int, message: Message) -> None:
+    if TYPE_CHECKING:
+        assert isinstance(message.guild, discord.Guild)
     await execute_query(
         "INSERT INTO menu_interfaces (menu_id, message_id) VALUES ($1, $2);",
         menu_id,
         message.id,
-        message.channel.id,
-        message.guild.id,
     )
 
 
-async def get_menus() -> List[Tuple[Any]]:
-    return [tuple(i) for i in await execute_read_query("SELECT * FROM menus;")]
+async def get_menus() -> List[Dict[str, Any]]:
+    return [dict(i) for i in await execute_read_query("SELECT * FROM menus;")]
 
 
-async def get_menus_user(*, user_id: int) -> List[Tuple[Any]]:
+async def get_menus_user(*, user_id: int) -> List[Dict[str, Any]]:
     return [
-        tuple(i)
+        dict(i)
         for i in await execute_read_query(
             "SELECT * FROM menus WHERE owner_id = $1;", user_id
         )
