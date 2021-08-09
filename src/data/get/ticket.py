fb4b23a698c0fae638c6f1f1caaca08c84b4ca2e
Village05 <mr.village05@gmail.com>
2021-08-08 23:18:51 -0400

    Add ticket get functions

---
+++ src/data/get/ticket.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Dict, Union
+
+from src.data.query.ticket import (
+    query_ticket,
+    query_ticket_config,
+    query_ticket_config_by_category,
+)
+
+from ..query import (
+    query_ticket,
+    query_ticket_by_channel,
+    query_ticket_by_config,
+    query_ticket_by_guild,
+)
+
+__all__ = ("get_ticket", "get_ticket_config")
+
+if TYPE_CHECKING:
+    from typings import TicketConfigData, TicketData
+
+
+async def get_ticket(
+    *,
+    ticket_id: int = None,
+    channel_id: int = None,
+    config_id: int = None,
+    guild_id: int = None
+) -> TicketData:
+    if ticket_id is not None:
+        return await query_ticket(ticket_id)
+    if channel_id is not None:
+        return await query_ticket_by_channel(channel_id)
+    if config_id is not None:
+        return await query_ticket_by_config(config_id)
+    if guild_id is not None:
+        return await query_ticket_by_guild(guild_id)
+    raise ValueError("No arguments given")
+
+
+async def get_ticket_config(
+    *, config_id: int = None, category_id: int = None
+) -> TicketConfigData:
+    if config_id is not None:
+        return await query_ticket_config(config_id)
+    if category_id is not None:
+        return await query_ticket_config_by_category(category_id)
+    raise ValueError("No arguments given")
