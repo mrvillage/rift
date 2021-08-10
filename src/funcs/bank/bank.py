680f46d024d262d05e052649483a39e2668ed553
Village05 <mr.village05@gmail.com>
2021-08-09 20:15:33 -0400

    Fix withdraw note parameter name

--- src/funcs/bank/bank.py
+++ src/funcs/bank/bank.py
@@ -5,7 +5,7 @@ async def withdraw(*, transaction, receiver, note: str = None):
     transaction_data = transaction.get_data_withdraw()
     transaction_data["withtype"] = type(receiver).__name__
     if note is not None:
-        transaction_data["note"] = note
+        transaction_data["withnote"] = note
     transaction_data["withrecipient"] = receiver.name
     if bot.auth_token is not None:
         transaction_data["token"] = bot.auth_token
