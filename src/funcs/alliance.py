a305c5c12010aeee57cfc1bce3eb2f086ec389e0
Village05 <mr.village05@gmail.com>
2021-08-10 22:18:55 -0400

    Update and type search_alliance

--- src/funcs/alliance.py
+++ src/funcs/alliance.py
@@ -1,3 +1,9 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from asyncpg import exceptions
+import discord
 from discord.ext.commands import (
     Context,
     MemberConverter,
@@ -7,18 +13,18 @@ from discord.ext.commands import (
 )
 
 from ..data import get
+from ..data.classes import Alliance, Nation
 from ..errors import AllianceNotFoundError, LinkError
 from ..funcs.utils import convert_link
 from .link import get_link_user
 
 
-async def search_alliance(ctx: Context, search):
-    from ..data.classes import Alliance, Nation
-
+async def search_alliance(ctx: Context, search: str) -> Alliance:
     try:
         search = await convert_link(search)
     except LinkError:
         pass
+    user = None
     try:
         user = await MemberConverter().convert(ctx, search)
     except MemberNotFound:
@@ -26,7 +32,9 @@ async def search_alliance(ctx: Context, search):
             user = await UserConverter().convert(ctx, search)
         except UserNotFound:
             pass
-    if "user" in locals():
+    if user is not None:
+        if TYPE_CHECKING:
+            assert isinstance(user, (discord.User, discord.Member))
         try:
             return await Alliance.fetch(
                 (await Nation.fetch((await get_link_user(user.id))[1])).alliance_id
@@ -38,11 +46,12 @@ async def search_alliance(ctx: Context, search):
             return await Alliance.fetch(int(search))
         except KeyError:
             pass
+        except exceptions.DataError:
+            raise AllianceNotFoundError(search)
     alliance = await get.get_alliance(search)
     if alliance is not None:
         return await Alliance.fetch(alliance.id)
     nation = await get.get_nation(search)
-    if nation is not None:
-        if nation.alliance_id != 0:
-            return await Alliance.fetch(nation.alliance_id)
-    raise AllianceNotFoundError
+    if nation is not None and nation.alliance_id != 0:
+        return await Alliance.fetch(nation.alliance_id)
+    raise AllianceNotFoundError(search)
