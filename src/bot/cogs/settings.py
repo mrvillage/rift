d9867cdd5ece19206014698ec64a74e21197ae0f
Village05 <mr.village05@gmail.com>
2021-08-10 22:43:58 -0400

    Move rest of welcome stuff outside welcome_channels if statement

--- src/bot/cogs/settings.py
+++ src/bot/cogs/settings.py
@@ -104,32 +104,30 @@ class Settings(commands.Cog):
                         )
                     except discord.Forbidden:
                         continue
-            roles: list[discord.Role] = []
-            highest_role: discord.Role = member.guild.get_member(
-                self.bot.user.id
-            ).top_role
-            for role in settings.join_roles:
+        roles: list[discord.Role] = []
+        highest_role: discord.Role = member.guild.get_member(self.bot.user.id).top_role
+        for role in settings.join_roles:
+            role: discord.Role = member.guild.get_role(role)
+            if highest_role > role:
+                roles.append(role)
+        if roles:
+            await member.add_roles(*roles)
+        roles = []
+        if nation:
+            if settings.verified_nickname:
+                if member.guild.get_member(
+                    self.bot.user.id
+                ).guild_permissions.manage_nicknames:
+                    await settings.set_verified_nickname(member, nation)
+            roles = []
+            for role in settings.verified_roles:
                 role: discord.Role = member.guild.get_role(role)
                 if highest_role > role:
                     roles.append(role)
             if roles:
                 await member.add_roles(*roles)
             roles = []
-            if nation:
-                if settings.verified_nickname:
-                    if member.guild.get_member(
-                        self.bot.user.id
-                    ).guild_permissions.manage_nicknames:
-                        await settings.set_verified_nickname(member, nation)
-                roles = []
-                for role in settings.verified_roles:
-                    role: discord.Role = member.guild.get_role(role)
-                    if highest_role > role:
-                        roles.append(role)
-                if roles:
-                    await member.add_roles(*roles)
-                roles = []
-                ...  # implement the rest of the welcome stuff below, will need to set up alliance settings and embassies first though
+            ...  # implement the rest of the welcome stuff below, will need to set up alliance settings and embassies first though
 
     @commands.Cog.listener()
     async def on_member_update(self, before: discord.Member, after: discord.Member):
