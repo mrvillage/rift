bbe1b607b8d6ce7cdc7b706f1b30e4a8df926b9d
Village05 <mr.village05@gmail.com>
2021-08-29 23:32:33 -0400

    Update logs functions and logging

--- src/logs/add.py
+++ src/logs/add.py
@@ -1,22 +1,26 @@
+from __future__ import annotations
+
+from typing import Optional
+
 from ..data.db import execute_query
 
 
 async def insert_log(
-    time,
-    message_id,
-    channel_id,
-    guild_id,
-    author_id,
-    message_content,
-    qualified_name,
-    args,
-    kwargs,
-    success=None,
-    errormessage=None,
-):
+    time: str,
+    message_id: int,
+    channel_id: int,
+    guild_id: Optional[int],
+    author_id: int,
+    message_content: str,
+    qualified_name: Optional[str],
+    args: str,
+    kwargs: str,
+    success: bool = None,
+    error_message: str = None,
+) -> None:
     await execute_query(
         """
-        INSERT INTO commandlogs (datetime, messageid, channelid, guildid, authorid, messagecontent, qualifiedname, args, kwargs, success, errormessage)
+        INSERT INTO command_logs (datetime, message_id, channel_id, guild_id, author_id, message_content, qualified_name, args, kwargs, success, error_message)
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);
         """,
         time,
@@ -29,5 +33,5 @@ async def insert_log(
         args,
         kwargs,
         success,
-        errormessage,
+        error_message,
     )
--- src/logs/edit.py
+++ src/logs/edit.py
@@ -1,33 +1,37 @@
+from __future__ import annotations
+
+from typing import Optional
+
 from ..data.db import execute_query
 
 
 async def edit_log(
-    time,
-    message_id,
-    channel_id,
-    guild_id,
-    author_id,
-    message_content,
-    qualified_name,
-    success=None,
-    errormessage=None,
-):
+    datetime: str,
+    message_id: int,
+    channel_id: int,
+    guild_id: Optional[int],
+    author_id: int,
+    message_content: str,
+    qualified_name: Optional[str],
+    success: bool = None,
+    error_message: str = None,
+) -> None:
     await execute_query(
         """
-        UPDATE commandlogs
+        UPDATE command_logs
         SET
             success = $8,
-            errormessage =  $9
+            error_message =  $9
         WHERE
             datetime = $1 AND
-            messageid = $2 AND
-            channelid = $3 AND
-            guildid = $4 AND
-            authorid = $5 AND
-            messagecontent = $6 AND
-            qualifiedname = $7;
+            message_id = $2 AND
+            channel_id = $3 AND
+            guild_id = $4 AND
+            author_id = $5 AND
+            message_content = $6 AND
+            qualified_name = $7;
         """,
-        time,
+        datetime,
         message_id,
         channel_id,
         guild_id,
@@ -35,5 +39,5 @@ async def edit_log(
         message_content,
         qualified_name,
         success,
-        errormessage,
+        error_message,
     )
