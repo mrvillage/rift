b1414a2ecd8fa48e4f16ef3208557974a5a553ac
Village05 <mr.village05@gmail.com>
2021-08-29 23:52:56 -0400

    Update get.link

--- src/data/get/link.py
+++ src/data/get/link.py
@@ -1,59 +1,38 @@
-from ..db import execute_query, execute_read_query
+from __future__ import annotations
+
+from typing import Dict, List
+
+from ..query import link
+
+__all__ = (
+    "get_links",
+    "add_link",
+    "remove_link_user",
+    "remove_link_nation",
+    "get_link_user",
+    "get_link_nation",
+)
 
 
-async def get_links():
-    return await execute_read_query(
-        """
-        SELECT * FROM links;
-    """
-    )
+async def get_links() -> List[Dict[str, int]]:
+    return await link.query_links()
 
 
-async def add_link(user_id, nation_id):
-    return await execute_query(
-        f"""
-        INSERT INTO links
-        VALUES
-        ({user_id},{nation_id})
-    """
-    )
+async def add_link(user_id: int, nation_id: int, /) -> None:
+    await link.add_link(user_id, nation_id)
 
 
-async def remove_link_user(user_id):
-    return await execute_query(
-        f"""
-        DELETE FROM links
-        WHERE id = {user_id};
-    """
-    )
+async def remove_link_user(user_id: int, /) -> None:
+    await link.remove_link_user(user_id)
 
 
-async def remove_link_nation(nation_id):
-    return await execute_query(
-        f"""
-        DELETE FROM links
-        WHERE nation = {nation_id};
-    """
-    )
+async def remove_link_nation(nation_id: int, /) -> None:
+    await link.remove_link_nation(nation_id)
 
 
-async def get_link_user(user_id):
-    return (
-        await execute_read_query(
-            f"""
-        SELECT * FROM links
-        WHERE id = {user_id};
-    """
-        )
-    )[0]
+async def get_link_user(user_id: int, /) -> Dict[str, int]:
+    return (await link.query_link_user(user_id))[0]
 
 
-async def get_link_nation(nation_id):
-    return (
-        await execute_read_query(
-            f"""
-        SELECT * FROM links
-        WHERE nation = {nation_id};
-    """
-        )
-    )[0]
+async def get_link_nation(nation_id: int, /) -> Dict[str, int]:
+    return (await link.query_link_nation(nation_id))[0]
