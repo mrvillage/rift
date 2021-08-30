2cd848466d578752f2077b2ce03eb8ec477cf1b0
Village05 <mr.village05@gmail.com>
2021-08-30 11:02:13 -0400

    Update typings and fix bugs with settings commands

--- src/bot/cogs/settings.py
+++ src/bot/cogs/settings.py
@@ -35,8 +35,8 @@ class Settings(commands.Cog):
         help="View of modify server settings.",
         invoke_without_command=True,
     )
-    @commands.guild_only()
     @has_manage_permissions()
+    @commands.guild_only()
     async def server_settings(self, ctx: commands.Context):
         ...
 
@@ -47,6 +47,7 @@ class Settings(commands.Cog):
         enabled=False,
     )
     @has_manage_permissions()
+    @commands.guild_only()
     async def server_settings_purpose(self, ctx: commands.Context):
         ...
 
@@ -56,11 +57,15 @@ class Settings(commands.Cog):
         help="Modify the server's welcome message.",
     )
     @has_manage_permissions()
+    @commands.guild_only()
     async def server_settings_welcome_message(
         self, ctx: commands.Context, *, message: str = None
     ):
-        settings = await GuildSettings.fetch(str(ctx.guild.id), "welcome_settings")
-        message = message.strip("\n ")
+        if TYPE_CHECKING:
+            assert isinstance(ctx.guild, discord.Guild)
+        settings = await GuildSettings.fetch(ctx.guild.id, "welcome_settings")
+        if message:
+            message = message.strip("\n ")
         await settings.welcome_settings.set_(welcome_message=message)
         await ctx.reply(
             embed=funcs.get_embed_author_member(
@@ -75,11 +80,15 @@ class Settings(commands.Cog):
         help="Modify the server's verified nickname format.",
     )
     @has_manage_permissions()
+    @commands.guild_only()
     async def server_settings_verified_nickname(
         self, ctx: commands.Context, *, nickname: str = None
     ):
-        settings = await GuildSettings.fetch(str(ctx.guild.id), "welcome_settings")
-        message = nickname.strip("\n ")
+        if TYPE_CHECKING:
+            assert isinstance(ctx.guild, discord.Guild)
+        settings = await GuildSettings.fetch(ctx.guild.id, "welcome_settings")
+        if nickname:
+            nickname = nickname.strip("\n ")
         await settings.welcome_settings.set_(verified_nickname=nickname)
         await ctx.reply(
             embed=funcs.get_embed_author_member(
@@ -90,7 +99,7 @@ class Settings(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
-        # sourcery skip: merge-nested-ifs
+        # sourcery no-metrics skip: merge-nested-ifs
         if member.pending:
             return
         try:
@@ -109,19 +118,21 @@ class Settings(commands.Cog):
                     continue
                 try:
                     embed = settings.format_welcome_embed(member, bool(nation))
-                    await channel.send(embed=embed)
+                    await channel.send(embed=embed)  # type: ignore
                 except discord.Forbidden:
                     try:
-                        await channel.send(
+                        await channel.send(  # type: ignore
                             f"Something went wrong welcoming {member.mention} to the server!"
                         )
                     except discord.Forbidden:
                         continue
         roles: list[discord.Role] = []
-        highest_role: discord.Role = member.guild.get_member(self.bot.user.id).top_role
+        highest_role: discord.Role = member.guild.get_member(self.bot.user.id).top_role  # type: ignore
         if settings.join_roles is not None:
-            for role in settings.join_roles:
-                role: discord.Role = member.guild.get_role(role)
+            for role_id in settings.join_roles:
+                role: Optional[discord.Role] = member.guild.get_role(role_id)
+                if role is None:
+                    continue
                 if highest_role > role:
                     roles.append(role)
             if roles:
@@ -130,12 +141,14 @@ class Settings(commands.Cog):
         if nation:
             if settings.verified_nickname:
                 if member.guild.get_member(
-                    self.bot.user.id
-                ).guild_permissions.manage_nicknames:
+                    self.bot.user.id  # type: ignore
+                ).guild_permissions.manage_nicknames:  # type: ignore
                     await settings.set_verified_nickname(member, nation)
             if settings.verified_roles is not None:
-                for role in settings.verified_roles:
-                    role: discord.Role = member.guild.get_role(role)
+                for role_id in settings.verified_roles:
+                    role: Optional[discord.Role] = member.guild.get_role(role_id)
+                    if role is None:
+                        continue
                     if highest_role > role:
                         roles.append(role)
                 if roles:
