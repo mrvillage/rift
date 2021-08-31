c1ffc41a73ae3d373f2120ac32c247b66414869f
Village05 <mr.village05@gmail.com>
2021-08-30 23:38:17 -0400

    Update Fun cog

--- src/bot/cogs/fun.py
+++ src/bot/cogs/fun.py
@@ -1,14 +1,18 @@
+from __future__ import annotations
+
 from discord.ext import commands
+
+from ...ref import Rift
 
 
 class Fun(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: Rift):
         self.bot = bot
 
     @commands.command(name="toot")
-    async def link(self, ctx):
+    async def toot(self, ctx: commands.Context):
         await ctx.send("Toot tooooooot!")
 
 
-def setup(bot):
+def setup(bot: Rift):
     bot.add_cog(Fun(bot))
