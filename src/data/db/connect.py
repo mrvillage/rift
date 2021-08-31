0b213496be8c01c25d1b55810335b6674ae05954
Village05 <mr.village05@gmail.com>
2021-08-31 00:20:03 -0400

    Add SSL to database connection

--- src/data/db/connect.py
+++ src/data/db/connect.py
@@ -1,13 +1,23 @@
-from asyncio import get_event_loop
+from __future__ import annotations
 
-from asyncpg import create_pool
+from asyncio import get_event_loop
+from ssl import create_default_context
+from pathlib import Path
+
+from asyncpg import Pool, create_pool
 
 from ...env import DBHOST, DBNAME, DBPASSWORD, DBPORT, DBUSER
 
 
-async def _create_connection():
-    return await create_pool(
-        host=DBHOST, port=DBPORT, user=DBUSER, password=DBPASSWORD, database=DBNAME
+async def _create_connection() -> Pool:
+    path = Path(__file__).parent.parent.parent.parent / "db.crt"
+    return await create_pool(  # type: ignore
+        host=DBHOST,
+        port=DBPORT,
+        user=DBUSER,
+        password=DBPASSWORD,
+        database=DBNAME,
+        ssl=create_default_context(cafile=path),
     )
 
 
