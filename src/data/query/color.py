9dbff9e124bf5067f2afce78edfe98f662a01aab
mrvillage <mr.village05@gmail.com>
2021-09-05 12:01:55 -0400

    Color queries

--- src/data/query/color.py
+++ src/data/query/color.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
 import json
-from typing import Dict, Union
+from typing import Dict, List, Union
 
 from ..db import execute_read_query
 
-__all__ = ("query_color",)
+__all__ = ("query_color", "query_colors", "query_nation_color_counts")
 
 
 async def query_color(color: str) -> Dict[str, Union[int, str]]:
+    return {i["color"]: i for i in await query_colors()}[color]
+
+
+async def query_colors() -> List[Dict[str, Union[int, str]]]:
     return json.loads(
         dict(
             (
@@ -16,5 +20,14 @@ async def query_color(color: str) -> Dict[str, Union[int, str]]:
                     "SELECT colors FROM colors ORDER BY datetime DESC LIMIT 1;"
                 )
             )[0]
-        )["colors"][color]
+        )["colors"]
     )
+
+
+async def query_nation_color_counts() -> List[Dict[str, Union[int, str]]]:
+    return [
+        dict(i)
+        for i in await execute_read_query(
+            "SELECT color, COUNT(color) FROM nations GROUP BY color;"
+        )
+    ]
