464389c7cd99b5d2f90d0c8065b63938efcb74d2
mrvillage <mr.village05@gmail.com>
2021-09-05 01:04:10 -0400

    Update alliances offset and max pages

--- src/data/get/alliance.py
+++ src/data/get/alliance.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
-from typing import List
+from typing import TYPE_CHECKING, List
 
-from ..classes import Alliance
 from ..db import execute_read_query
-from ..query import query_alliances_offset
+from ..query import query_alliances_offset, query_max_alliances_page
+
+if TYPE_CHECKING:
+    from ..classes import Alliance
 
 
 async def get_alliance(search):
@@ -23,7 +25,16 @@ async def get_alliance(search):
 
 
 async def get_alliances_offset(*, limit: int = 50, offset: int = 0) -> List[Alliance]:
+    from ..classes import Alliance
+
     return [
         Alliance(data=list(i.values()))
         for i in await query_alliances_offset(limit=limit, offset=offset)
     ]
+
+
+async def get_max_alliances_page() -> int:
+    value = await query_max_alliances_page()
+    if value - (value // 50):
+        return value // 50 + 1
+    return value // 50
