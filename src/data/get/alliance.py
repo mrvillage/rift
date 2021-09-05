de434ff083100ce27e8e821a3b4129e26edc1ae7
mrvillage <mr.village05@gmail.com>
2021-09-04 23:45:37 -0400

    Add get_alliances_offset

--- src/data/get/alliance.py
+++ src/data/get/alliance.py
@@ -1,4 +1,10 @@
+from __future__ import annotations
+
+from typing import List
+
+from ..classes import Alliance
 from ..db import execute_read_query
+from ..query import query_alliances_offset
 
 
 async def get_alliance(search):
@@ -14,3 +20,10 @@ async def get_alliance(search):
     )
     if alliance:
         return await Alliance.fetch(alliance[0][0])
+
+
+async def get_alliances_offset(*, limit: int = 50, offset: int = 0) -> List[Alliance]:
+    return [
+        Alliance(data=list(i.values()))
+        for i in await query_alliances_offset(limit=limit, offset=offset)
+    ]
