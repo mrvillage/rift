d29d747337decc4675afb4d60cf251feb217f5b7
mrvillage <mr.village05@gmail.com>
2021-09-08 22:41:38 -0400

    Implement cache class

--- src/cache.py
+++ src/cache.py
@@ -1,5 +1,414 @@
-nations = {}
-cities = {}
-alliances = {}
-documents = {}
-servers = {}
+from __future__ import annotations
+
+import asyncio
+import json
+from typing import Any, Dict, List, Literal, Optional, Set, Type
+
+from typings import AllianceData, Link
+
+from .data.classes import *
+from .data.db import execute_read_query
+
+__all__ = ("cache",)
+
+
+class Validate:
+    alliances: bool = False
+    cities: bool = False
+    colors: bool = False
+    nations: bool = False
+    prices: bool = False
+    treasures: bool = False
+    treaties: bool = False
+
+
+class Cache:
+    __slots__ = (
+        "_alliances",
+        "_cities",
+        "_colors",
+        "_embassies",
+        "_embassy_configs",
+        "_guild_settings",
+        "_guild_welcome_settings",
+        "_links",
+        "_menu_interfaces",
+        "_menu_items",
+        "_menus",
+        "_nations",
+        "_prices",
+        "_ticket_configs",
+        "_tickets",
+        "_trades",
+        "_treasures",
+        "_treaties",
+        "_user_settings",
+        "_war_attacks",
+        "_wars",
+        "init",
+        "validate",
+    )
+
+    def __init__(self):
+        self._alliances: Dict[int, Alliance] = {}
+        self._cities: Dict[int, City] = {}
+        self._colors: Dict[str, Color] = {}
+        self._embassies: Dict[int, Embassy] = {}
+        self._embassy_configs: Dict[int, EmbassyConfig] = {}
+        self._guild_settings: Dict[int, GuildSettings] = {}
+        self._guild_welcome_settings: Dict[int, GuildWelcomeSettings] = {}
+        self._links: List[Link] = list()
+        self._menu_interfaces: List[Dict[str, int]] = list()
+        self._menu_items: Dict[int, MenuItem] = {}
+        self._menus: Dict[int, Menu] = {}
+        self._nations: Dict[int, Nation] = {}
+        self._prices: TradePrices
+        self._ticket_configs: Dict[int, TicketConfig] = {}
+        self._tickets: Dict[int, Ticket] = {}
+        self._trades = {}  # NO CLASS YET
+        self._treasures = list()  # NO CLASS YET
+        self._treaties: List[Treaty] = list()
+        self._user_settings: Dict[int, UserSettings] = {}
+        self._war_attacks = {}  # NO CLASS YET
+        self._wars = {}  # NO CLASS YET
+        self.init: bool = False
+        self.validate: Type[Validate] = Validate
+
+    async def initialize(self):
+        queries = [
+            "SELECT * FROM alliances;",
+            "SELECT * FROM cities;",
+            "SELECT * FROM colors ORDER BY datetime DESC LIMIT 1;",
+            "SELECT * FROM embassies;",
+            "SELECT * FROM embassy_configs;",
+            "SELECT * FROM guild_settings;",
+            "SELECT * FROM guild_welcome_settings;",
+            "SELECT * FROM links;",
+            "SELECT * FROM menu_interfaces;",
+            "SELECT * FROM menu_items;",
+            "SELECT * FROM menus;",
+            "SELECT * FROM nations;",
+            "SELECT * FROM prices ORDER BY datetime DESC LIMIT 1;",
+            "SELECT * FROM ticket_configs;",
+            "SELECT * FROM tickets;",
+            "SELECT * FROM treaties;",
+        ]
+        (
+            alliances,
+            cities,
+            colors,
+            embassies,
+            embassy_configs,
+            guild_settings,
+            guild_welcome_settings,
+            links,
+            menu_interfaces,
+            menu_items,
+            menus,
+            nations,
+            prices,
+            ticket_configs,
+            tickets,
+            treaties,
+        ) = tuple(
+            await asyncio.gather(*(execute_read_query(query) for query in queries))
+        )
+        for i in alliances:
+            i = Alliance(i)
+            self._alliances[i.id] = i
+        for i in cities:
+            i = City(i)
+            self._cities[i.id] = i
+        for i in colors[0]["colors"]:  # SPECIAL CASE
+            i = Color(i)
+            self._colors[i.name] = i
+        for i in embassies:
+            i = Embassy(i)
+            self._embassies[i.embassy_id] = i
+        for i in embassy_configs:
+            i = EmbassyConfig(i)
+            self._embassy_configs[i.config_id] = i
+        for i in guild_settings:
+            i = GuildSettings(i)
+            self._guild_settings[i.guild_id] = i
+        for i in guild_welcome_settings:
+            i = GuildWelcomeSettings(i)
+            self._guild_welcome_settings[i.guild_id] = i
+        for i in links:
+            self._links.append(dict(i))  # type: ignore
+        for i in menu_interfaces:
+            self._menu_interfaces.append(dict(i))  # type: ignore
+        for i in menu_items:
+            i = MenuItem(i)
+            self._menu_items[i.item_id] = i
+        for i in menus:
+            i = Menu(i)
+            self._menus[i.menu_id] = i
+        for i in nations:
+            i = Nation(i)
+            self._nations[i.id] = i
+        # for some reason is the only JSON type to give string, so did compatible with proper loading fix here
+        self._prices = TradePrices(
+            {
+                key: json.loads(value)
+                if key != "datetime" and isinstance(value, str)
+                else value
+                for key, value in dict(prices[0]).items()
+            }
+        )
+        for i in ticket_configs:
+            i = TicketConfig(i)
+            self._ticket_configs[i.config_id] = i
+        for i in tickets:
+            i = Ticket(i)
+            self._tickets[i.ticket_id] = i
+        for i in treaties:
+            i = Treaty(
+                i,
+                {
+                    i["from_"]: self.get_alliance(i["from_"]),  # type: ignore
+                    i["to_"]: self.get_alliance(i["to_"]),  # type: ignore
+                },
+            )
+            self._treaties.append(i)
+        self.init = True
+
+    @property
+    def alliances(self) -> Set[Alliance]:
+        return set(self._alliances.values())
+
+    @property
+    def cities(self) -> Set[City]:
+        return set(self._cities.values())
+
+    @property
+    def colors(self) -> Set[Color]:
+        return set(self._colors.values())
+
+    @property
+    def embassies(self) -> Set[Embassy]:
+        return set(self._embassies.values())
+
+    @property
+    def embassy_configs(self) -> Set[EmbassyConfig]:
+        return set(self._embassy_configs.values())
+
+    @property
+    def guild_settings(self) -> Set[GuildSettings]:
+        return set(self._guild_settings.values())
+
+    @property
+    def guild_welcome_settings(self) -> Set[GuildWelcomeSettings]:
+        return set(self._guild_welcome_settings.values())
+
+    @property
+    def links(self) -> List[Link]:
+        return self._links
+
+    @property
+    def menu_interfaces(self) -> List[Dict[str, int]]:
+        return self._menu_interfaces
+
+    @property
+    def menu_items(self) -> Set[MenuItem]:
+        return set(self._menu_items.values())
+
+    @property
+    def menus(self) -> Set[Menu]:
+        return set(self._menus.values())
+
+    @property
+    def nations(self) -> Set[Nation]:
+        return set(self._nations.values())
+
+    @property
+    def prices(self) -> TradePrices:
+        return self._prices
+
+    @property
+    def ticket_configs(self) -> Set[TicketConfig]:
+        return set(self._ticket_configs.values())
+
+    @property
+    def tickets(self) -> Set[Ticket]:
+        return set(self._tickets.values())
+
+    @property
+    def trades(self) -> Set[int]:
+        return set(self._trades.values())
+
+    @property
+    def treasures(self) -> List[int]:
+        return self._treasures
+
+    @property
+    def treaties(self) -> List[Treaty]:
+        return self._treaties
+
+    @property
+    def user_settings(self) -> Set[UserSettings]:
+        return set(self._user_settings.values())
+
+    @property
+    def war_attacks(self) -> Set[int]:
+        return set(self._war_attacks.values())
+
+    @property
+    def wars(self) -> Set[int]:
+        return set(self._wars.values())
+
+    def hook_alliance(
+        self, action: Literal["update", "create", "delete"], data: AllianceData
+    ) -> None:
+        if action == "delete":
+            del self._alliances[data["id"]]
+            return
+        try:
+            self._alliances[data["id"]]._update(data)
+        except KeyError:
+            self._alliances[data["id"]] = Alliance(data)
+
+    def hook_city(
+        self, action: Literal["update", "create", "delete"], data: Dict[Any, Any]
+    ) -> None:
+        if action == "delete":
+            del self._cities[data["id"]]
+            return
+        try:
+            self._cities[data["id"]]._update(data)
+        except KeyError:
+            self._cities[data["id"]] = City(data)
+
+    def hook_color(self, action: Literal["update"], data: Dict[Any, Any]) -> None:
+        try:
+            self._colors[data["color"]]._update(data)
+        except KeyError:
+            self._colors[data["color"]] = Color(data)
+
+    def hook_nation(
+        self, action: Literal["update", "create", "delete"], data: Dict[Any, Any]
+    ) -> None:
+        if action == "delete":
+            del self._nations[data["id"]]
+            return
+        try:
+            self._nations[data["id"]]._update(data)
+        except KeyError:
+            self._nations[data["id"]] = Nation(data)
+
+    def hook_price(self, action: Literal["update"], data: Dict[Any, Any]) -> None:
+        try:
+            self._prices._update(data)
+        except KeyError:
+            self._prices._update(data)
+
+    def hook_treasure(self, action: Literal["update"], data: Dict[Any, Any]) -> None:
+        for new in data.values():
+            next(i._update(new) for i in self._treasures if i.name == new["name"])
+
+    def hook_treaty(
+        self, action: Literal["create", "delete"], data: Dict[Any, Any]
+    ) -> None:
+        if action == "delete":
+            treaty = next(
+                i
+                for i in self._treaties
+                if i.from_ == data["from_"] and i.to_ == data["to_"]
+            )
+            self._treaties.remove(treaty)
+        alliances = {
+            data["from_"]: self.get_alliance(data["from_"]),
+            data["to_"]: self.get_alliance(data["to_"]),
+        }
+        self._treaties.append(Treaty(list(data.values()), alliances))  # type: ignore
+
+    def get_alliance(self, id: int, /) -> Optional[Alliance]:
+        return self._alliances.get(id)
+
+    def get_city(self, id: int, /) -> Optional[City]:
+        return self._cities.get(id)
+
+    def get_color(self, name: str, /) -> Optional[Color]:
+        return self._colors.get(name)
+
+    def get_embassy(self, id: int, /) -> Optional[Embassy]:
+        return self._embassies.get(id)
+
+    def get_embassy_config(self, id: int, /) -> Optional[EmbassyConfig]:
+        return self._embassy_configs.get(id)
+
+    def get_guild_settings(self, id: int, /) -> Optional[GuildSettings]:
+        return self._guild_settings.get(id)
+
+    def get_guild_welcome_settings(self, id: int, /) -> Optional[GuildWelcomeSettings]:
+        return self._guild_welcome_settings.get(id)
+
+    def get_link(self, id: int, /) -> Optional[Link]:
+        try:
+            return next(
+                i for i in self._links if i["user_id"] == id or i["nation_id"] == id
+            )
+        except StopIteration:
+            return
+
+    def get_menu_interface(
+        self, menu_id: int, message_id: int, /
+    ) -> Optional[Dict[str, int]]:
+        try:
+            return next(
+                i
+                for i in self._menu_interfaces
+                if i["menu_id"] == menu_id and i["message_id"] == message_id
+            )
+        except StopIteration:
+            return
+
+    def get_menu_item(self, id: int, guild_id: int, /) -> Optional[MenuItem]:
+        item = self._menu_items.get(id)
+        if item is None:
+            return
+        if item.guild_id != guild_id:
+            return
+        return item
+
+    def get_menu(self, id: int, /) -> Optional[Menu]:
+        return self._menus.get(id)
+
+    def get_nation(self, id: int, /) -> Optional[Nation]:
+        return self._nations[id]
+
+    def get_prices(self) -> Optional[TradePrices]:
+        return self._prices
+
+    def get_ticket_config(self, id: int, /) -> Optional[TicketConfig]:
+        return self._ticket_configs.get(id)
+
+    def get_ticket(self, id: int, /) -> Optional[Ticket]:
+        return self._tickets.get(id)
+
+    def get_trade(self, id: int, /) -> Optional[int]:
+        return self._trades.get(id)
+
+    def get_treasure(self, name: str, /) -> Optional[int]:
+        try:
+            return next(i for i in self._treasures if i.name == name)
+        except StopIteration:
+            return
+
+    def get_treaty(self, from_: int, to_: int, /) -> Optional[Treaty]:
+        try:
+            return next(i for i in self._treaties if i.from_ == from_ and i.to_ == to_)
+        except StopIteration:
+            return
+
+    def get_user_settings(self, id: int, /) -> Optional[UserSettings]:
+        return self._user_settings.get(id)
+
+    def get_war_attack(self, id: int, /) -> Optional[int]:
+        return self._war_attacks.get(id)
+
+    def get_war(self, id: int, /) -> Optional[int]:
+        return self._wars.get(id)
+
+
+cache = Cache()
