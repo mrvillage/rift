9511c2a805db631a19f02df8a421d7af743ea1f2
mrvillage <mr.village05@gmail.com>
2021-09-09 12:32:55 -0400

    Add inline typings for prices classes

--- src/data/classes/trade.py
+++ src/data/classes/trade.py
@@ -12,11 +12,11 @@ class TradeOffer:
     __slots__ = ("data", "datetime", "nation_id", "amount", "price", "total_value")
 
     def __init__(self, data: TradeOfferData):
-        self.datetime = datetime.fromisoformat(data["date"])
-        self.nation_id = int(data["nationid"])
-        self.amount = int(data["amount"])
-        self.price = int(data["price"])
-        self.total_value = int(data["totalvalue"])
+        self.datetime: datetime = datetime.fromisoformat(data["date"])
+        self.nation_id: int = int(data["nationid"])
+        self.amount: int = int(data["amount"])
+        self.price: int = int(data["price"])
+        self.total_value: int = int(data["totalvalue"])
 
 
 class ResourcePrice:
@@ -32,13 +32,13 @@ class ResourcePrice:
     )
 
     def __init__(self, data: ResourcePriceData):
-        self.name = data["resource"]
-        self.average_price = int(data["avgprice"])
-        self.avg_price = self.average_price
-        self.market_index = int(data["marketindex"].replace(",", ""))
-        self.highest_buy = TradeOffer(data["highestbuy"])
-        self.lowest_sell = TradeOffer(data["lowestbuy"])
-        self.trade_margin = self.lowest_sell.price - self.highest_buy.price
+        self.name: str = data["resource"]
+        self.average_price: int = int(data["avgprice"])
+        self.avg_price: int = self.average_price
+        self.market_index: int = int(data["marketindex"].replace(",", ""))
+        self.highest_buy: TradeOffer = TradeOffer(data["highestbuy"])
+        self.lowest_sell: TradeOffer = TradeOffer(data["lowestbuy"])
+        self.trade_margin: int = self.lowest_sell.price - self.highest_buy.price
 
 
 class TradePrices:
@@ -60,19 +60,19 @@ class TradePrices:
     )
 
     def __init__(self, data: TradePriceData):
-        self.credit = ResourcePrice(data["credit"])
-        self.coal = ResourcePrice(data["coal"])
-        self.oil = ResourcePrice(data["oil"])
-        self.uranium = ResourcePrice(data["uranium"])
-        self.lead = ResourcePrice(data["lead"])
-        self.iron = ResourcePrice(data["iron"])
-        self.bauxite = ResourcePrice(data["bauxite"])
-        self.gasoline = ResourcePrice(data["gasoline"])
-        self.munitions = ResourcePrice(data["munitions"])
-        self.steel = ResourcePrice(data["steel"])
-        self.aluminum = ResourcePrice(data["aluminum"])
-        self.food = ResourcePrice(data["food"])
-        self.market_index = self.credit.market_index
+        self.credit: ResourcePrice = ResourcePrice(data["credit"])
+        self.coal: ResourcePrice = ResourcePrice(data["coal"])
+        self.oil: ResourcePrice = ResourcePrice(data["oil"])
+        self.uranium: ResourcePrice = ResourcePrice(data["uranium"])
+        self.lead: ResourcePrice = ResourcePrice(data["lead"])
+        self.iron: ResourcePrice = ResourcePrice(data["iron"])
+        self.bauxite: ResourcePrice = ResourcePrice(data["bauxite"])
+        self.gasoline: ResourcePrice = ResourcePrice(data["gasoline"])
+        self.munitions: ResourcePrice = ResourcePrice(data["munitions"])
+        self.steel: ResourcePrice = ResourcePrice(data["steel"])
+        self.aluminum: ResourcePrice = ResourcePrice(data["aluminum"])
+        self.food: ResourcePrice = ResourcePrice(data["food"])
+        self.market_index: int = self.credit.market_index
 
     def _update(self, data, /) -> TradeOffer:
         ...
