49019743339625216b0bd5be7241278e71e78d2c
mrvillage <mr.village05@gmail.com>
2021-09-09 18:39:22 -0400

    Update get_trade_prices for proper loading

--- src/funcs/trade.py
+++ src/funcs/trade.py
@@ -1,7 +1,20 @@
+from __future__ import annotations
+
+import json
+
 from ..data.get import get_prices
+
+__all__ = ("get_trade_prices",)
 
 
 async def get_trade_prices():
     from ..data.classes import TradePrices
 
-    return TradePrices(await get_prices())
+    return TradePrices(
+        {  # type: ignore
+            key: json.loads(value)
+            if key != "datetime" and isinstance(value, str)
+            else value
+            for key, value in (await get_prices()).items()
+        }
+    )
