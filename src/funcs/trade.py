8567c90340531466e10b7f6811e745c957f7262e
Village05 <mr.village05@gmail.com>
2021-09-11 23:14:26 -0400

    Update get_trade_prices to use cache

--- src/funcs/trade.py
+++ src/funcs/trade.py
@@ -1,20 +1,9 @@
 from __future__ import annotations
 
-import json
-
-from ..data.get import get_prices
+from ..cache import cache
 
 __all__ = ("get_trade_prices",)
 
 
 async def get_trade_prices():
-    from ..data.classes import TradePrices
-
-    return TradePrices(
-        {  # type: ignore
-            key: json.loads(value)
-            if key != "datetime" and isinstance(value, str)
-            else value
-            for key, value in (await get_prices()).items()
-        }
-    )
+    return cache.prices
