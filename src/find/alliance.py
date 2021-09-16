37eaff92449bcf4d66eacc8869cac7d8bcdc3b3b
Village05 <mr.village05@gmail.com>
2021-09-15 21:46:10 -0400

    Typehint find.search_alliance

--- src/find/alliance.py
+++ src/find/alliance.py
@@ -1,6 +1,17 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from discord.ext import commands
+
 from .. import funcs
 
+__all__ = ("search_alliance",)
 
-async def search_alliance(ctx, search):
-    search = str(ctx.author.id) if search is None else search
+if TYPE_CHECKING:
+    from ..data.classes import Alliance
+
+
+async def search_alliance(ctx: commands.Context, search: str) -> Alliance:
+    search = search or str(ctx.author.id)
     return await funcs.search_alliance(ctx, search)
