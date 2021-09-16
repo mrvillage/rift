0f0600045b95b4728a68edbc360bab541a9c5b73
Village05 <mr.village05@gmail.com>
2021-09-15 22:37:04 -0400

    Update and modernize find.nation

--- src/find/nation.py
+++ src/find/nation.py
@@ -1,24 +1,27 @@
-from discord.ext.commands import UserConverter, UserNotFound
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Tuple, Union
+
+import discord
+from discord.ext import commands
 
 from .. import funcs
 
+if TYPE_CHECKING:
+    from ..data.classes import Nation
 
-async def search_nation_author(ctx, search):
-    author = ctx.author if search is None else None
+
+async def search_nation_author(
+    ctx: commands.Context, search: str
+) -> Tuple[Union[discord.User, discord.Member, discord.Guild], Nation]:
     search = str(ctx.author.id) if search is None else search
     nation = await funcs.search_nation(ctx, search)
-    if author is None:
-        try:
-            author = await funcs.get_link_nation(nation.id)
-            try:
-                author = await UserConverter().convert(ctx, str(author))
-            except UserNotFound:
-                author = ctx.author if ctx.guild is None else ctx.guild
-        except IndexError:
-            author = ctx.author if ctx.guild is None else ctx.guild
-    return author, nation
+    if nation.user is None:
+        return ctx.author if ctx.guild is None else ctx.guild, nation
+    else:
+        return nation.user, nation
 
 
-async def search_nation(ctx, search):
+async def search_nation(ctx: commands.Context, search: str) -> Nation:
     search = str(ctx.author.id) if search is None else search
     return await funcs.search_nation(ctx, search)
