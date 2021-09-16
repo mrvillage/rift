4a871a99d4ce6a67b79399f7b08140abe00ed688
Village05 <mr.village05@gmail.com>
2021-09-16 10:17:11 -0400

    Add "fuzzy" searching for nations

--- src/funcs/nation.py
+++ src/funcs/nation.py
@@ -1,40 +1,27 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from discord.ext import commands
 
-import discord
-from asyncpg import exceptions
-from discord.ext.commands import (
-    Context,
-    MemberConverter,
-    MemberNotFound,
-    UserConverter,
-    UserNotFound,
-)
-
-from ..data import get
+from ..cache import cache
 from ..data.classes import Nation
 from ..errors import LinkError, NationNotFoundError
 from ..funcs.utils import convert_link
 from .link import get_link_user
 
 
-async def search_nation(ctx: Context, search: str) -> Nation:
+async def search_nation(ctx: commands.Context, search: str) -> Nation:
     try:
         search = await convert_link(search)
     except LinkError:
         pass
-    user = None
     try:
-        user = await MemberConverter().convert(ctx, search)
-    except MemberNotFound:
+        user = await commands.MemberConverter().convert(ctx, search)
+    except commands.MemberNotFound:
         try:
-            user = await UserConverter().convert(ctx, search)
-        except UserNotFound:
-            pass
+            user = await commands.UserConverter().convert(ctx, search)
+        except commands.UserNotFound:
+            user = None
     if user is not None:
-        if TYPE_CHECKING:
-            assert isinstance(user, (discord.User, discord.Member))
         try:
             return await Nation.fetch((await get_link_user(user.id))["nation_id"])
         except IndexError:
@@ -44,9 +31,73 @@ async def search_nation(ctx: Context, search: str) -> Nation:
             return await Nation.fetch(int(search))
         except KeyError:
             pass
-        except exceptions.DataError:
-            raise NationNotFoundError(search)
-    nation = await get.get_nation(search)
-    if nation is not None:
-        return nation
+    nations = cache.nations
+    # full name search, case insensitive
+    if len(l := [i for i in nations if i.name.lower() == search.lower()]) == 1:
+        return l[0]
+    # leader name search, case insensitive
+    if len(l := [i for i in nations if i.leader.lower() == search.lower()]) == 1:
+        return l[0]
+    # full name search, case insensitive, no "the"
+    if (
+        len(
+            l := [
+                i
+                for i in nations
+                if i.name.lower().replace("the ", "")
+                == search.lower().replace("the ", "")
+            ]
+        )
+        == 1
+    ):
+        return l[0]
+    # startswith and endswith name search, case insensitive
+    if (
+        len(
+            l := [
+                i
+                for i in nations
+                if i.name.lower().startswith(search.lower())
+                or i.name.lower().endswith(search.lower())
+            ]
+        )
+        == 1
+    ):
+        return l[0]
+    # startswith and endswith name search, case insensitive, no "the"
+    if (
+        len(
+            l := [
+                i
+                for i in nations
+                if i.name.lower()
+                .replace("the ", "")
+                .startswith(search.lower().replace("the ", ""))
+                or i.name.lower()
+                .replace("the ", "")
+                .endswith(search.lower().replace("the ", ""))
+            ]
+        )
+        == 1
+    ):
+        return l[0]
+    # startswith and endswith leader search, case insensitive
+    if (
+        len(
+            l := [
+                i
+                for i in nations
+                if i.leader.lower().startswith(search.lower())
+                or i.leader.lower().endswith(search.lower())
+            ]
+        )
+        == 1
+    ):
+        return l[0]
+    # partial name search
+    if len(l := [i for i in nations if search.lower() in i.name.lower()]) == 1:
+        return l[0]
+    # partial leader search
+    if len(l := [i for i in nations if search.lower() in i.leader.lower()]) == 1:
+        return l[0]
     raise NationNotFoundError(search)
