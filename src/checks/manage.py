ebf6df042b9ec790c22971b27be8b327f10a13cc
mrvillage <mr.village05@gmail.com>
2021-09-16 22:45:50 -0400

    Update has_manage_permissions check to raise an error on fail

--- src/checks/manage.py
+++ src/checks/manage.py
@@ -4,10 +4,12 @@ from discord.ext import commands
 def has_manage_permissions():
     async def predicate(ctx: commands.Context):
         perms = ctx.author.guild_permissions
-        return (
+        if (
             perms.manage_guild
             or perms.administrator
             or ctx.guild.owner_id == ctx.author.id
-        )
+        ):
+            return True
+        raise commands.MissingPermissions(["manage"])
 
     return commands.check(predicate)
