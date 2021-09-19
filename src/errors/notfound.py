becfb8dc19fad326780b48208646f1276bc53de4
mrvillage <mr.village05@gmail.com>
2021-09-19 11:58:41 -0400

    Add TargetNotFoundError

--- src/errors/notfound.py
+++ src/errors/notfound.py
@@ -14,6 +14,7 @@ __all__ = (
     "NationNotFoundError",
     "RecipientNotFoundError",
     "ServerNotFoundError",
+    "TargetNotFoundError",
 )
 
 
@@ -70,4 +71,8 @@ class RecipientNotFoundError(NotFoundError):
 
 
 class ServerNotFoundError(NotFoundError):
+    ...
+
+
+class TargetNotFoundError(NotFoundError):
     ...
