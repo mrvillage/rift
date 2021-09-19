e250dddb563cf8cfd1d87fff0fb6264b5160ac3a
mrvillage <mr.village05@gmail.com>
2021-09-18 22:47:10 -0400

    Update tools command groupings

--- src/bot/cogs/tools.py
+++ src/bot/cogs/tools.py
@@ -17,32 +17,66 @@ class Tools(commands.Cog):
         ...
 
     @tools.command(name="infrastructure", type=commands.CommandType.chat_input)
-    async def tools_infrastructure(self, ctx: commands.Context, *, before: float, after: float, urbanization_policy: bool = False, center_for_civil_engineering: bool = False, advanced_engineering_corps: bool = False):
+    async def tools_infrastructure(
+        self,
+        ctx: commands.Context,
+        *,
+        before: float,
+        after: float,
+        urbanization_policy: bool = False,
+        center_for_civil_engineering: bool = False,
+        advanced_engineering_corps: bool = False
+    ):
         ...
 
     @tools.command(name="land", type=commands.CommandType.chat_input)
-    async def tools_land(self, ctx: commands.Context, *, before: float, after: float, rapid_expansion_policy: bool = False, arable_land_agency: bool = False, advanced_engineering_corps: bool = False):
+    async def tools_land(
+        self,
+        ctx: commands.Context,
+        *,
+        before: float,
+        after: float,
+        rapid_expansion_policy: bool = False,
+        arable_land_agency: bool = False,
+        advanced_engineering_corps: bool = False
+    ):
         ...
 
     @tools.command(name="city", type=commands.CommandType.chat_input)
-    async def tools_city(self, ctx: commands.Context, *, before: int, after: int, manifest_destiny_policy: bool = False, urban_planning: bool = False, advanced_urban_planning: bool = False):
+    async def tools_city(
+        self,
+        ctx: commands.Context,
+        *,
+        before: int,
+        after: int,
+        manifest_destiny_policy: bool = False,
+        urban_planning: bool = False,
+        advanced_urban_planning: bool = False
+    ):
         ...
 
     @tools.group(name="nation", type=commands.CommandType.chat_input)
     async def tools_nation(self, ctx: commands.Context):
         ...
 
-    @tools.command(name="infrastructure", type=commands.CommandType.chat_input)
-    async def tools_nation_infrastructure(self, ctx: commands.Context, *, nation: Nation, after: float):
+    @tools_nation.command(name="infrastructure", type=commands.CommandType.chat_input)
+    async def tools_nation_infrastructure(
+        self, ctx: commands.Context, *, nation: Nation, after: float
+    ):
         ...
 
-    @tools.command(name="land", type=commands.CommandType.chat_input)
-    async def tools_nation_land(self, ctx: commands.Context, *, nation: Nation, after: float):
+    @tools_nation.command(name="land", type=commands.CommandType.chat_input)
+    async def tools_nation_land(
+        self, ctx: commands.Context, *, nation: Nation, after: float
+    ):
         ...
 
-    @tools.command(name="city", type=commands.CommandType.chat_input)
-    async def tools_nation_city(self, ctx: commands.Context, *, nation: Nation, after: int):
+    @tools_nation.command(name="city", type=commands.CommandType.chat_input)
+    async def tools_nation_city(
+        self, ctx: commands.Context, *, nation: Nation, after: int
+    ):
         ...
+
 
 def setup(bot: Rift):
     bot.add_cog(Tools(bot))
