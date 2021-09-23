4bb3d84ea3307f1e34206414ffca6e092abc8b89
mrvillage <mr.village05@gmail.com>
2021-09-23 18:52:29 -0400

    Add SubscriptionData

---
+++ typings/data/subscription.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from typing import List, Literal, TypedDict
+
+__all__ = ("SubscriptionData", "EventCategoryLiteral", "EventTypeLiteral")
+
+EventCategoryLiteral = Literal[
+    "ALLIANCE", "FORUM", "NATION"
+]  # ATTACK, COLOR, PRICE, TRADE, TREASURE, WAR
+EventTypeLiteral = Literal["CREATE", "DELETE", "UPDATE"]  # ACCEPT, VICTORY
+
+
+class SubscriptionData(TypedDict):
+    id: int
+    token: str
+    guild_id: int
+    channel_id: int
+    category: EventCategoryLiteral
+    type: EventTypeLiteral
+    sub_types: List[str]
+    arguments: List[int]
