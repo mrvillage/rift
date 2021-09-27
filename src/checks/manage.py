dad2d38be5ec81a022b921489691d3ff6daf3db0
mrvillage <mr.village05@gmail.com>
2021-09-26 21:34:40 -0400

    Update typing in has_manage_permissions

--- src/checks/manage.py
+++ src/checks/manage.py
@@ -1,8 +1,17 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from discord.ext import commands
 
+if TYPE_CHECKING:
+    import discord
 
 def has_manage_permissions():
     async def predicate(ctx: commands.Context):
+        if TYPE_CHECKING:
+            assert isinstance(ctx.author, discord.Member)
+            assert isinstance(ctx.guild, discord.Guild)
         perms = ctx.author.guild_permissions
         if ctx.bot.enable_debug and await ctx.bot.is_owner(ctx.author):
             return True
