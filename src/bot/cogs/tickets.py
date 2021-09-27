8b95545b40dac197a5bf2f4f57dcb72d1236ecb9
mrvillage <mr.village05@gmail.com>
2021-09-26 21:24:56 -0400

    Update embassy and ticket config list to stop filtering configs where category is None

--- src/bot/cogs/tickets.py
+++ src/bot/cogs/tickets.py
@@ -155,10 +155,9 @@ class Tickets(commands.Cog):
             embed=funcs.get_embed_author_member(
                 ctx.author,
                 "\n".join(
-                    f"{config.id} - {category}"
+                    f"{config.id} - {ctx.guild.get_channel(config.category_id)}"
                     for config in configs
                     if config.category_id
-                    and (category := ctx.guild.get_channel(config.category_id))
                 ),
                 color=discord.Color.green(),
             )
