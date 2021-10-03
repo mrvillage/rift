d5dfd4d62b25ce9e2ad34cd94b95d7b5f14cfcaa
mrvillage <mr.village05@gmail.com>
2021-10-02 22:30:48 -0400

    Rename typings to _typings to prevent typeshed issues

---
+++ _typings/_discord.py
@@ -0,0 +1,9 @@
+from typing import TypeVar, Union
+
+from discord.embeds import _EmptyEmbed  # type: ignore
+
+__all__ = ("MaybeEmptyEmbed",)
+
+T = TypeVar("T")
+
+MaybeEmptyEmbed = Union[T, _EmptyEmbed]
