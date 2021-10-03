fd58e6650c2a4e3762299bff82d4ea054e1cd749
mrvillage <mr.village05@gmail.com>
2021-10-03 00:31:34 -0400

    Update _typings

---
+++ _typings/__init__.py
@@ -0,0 +1,3 @@
+from .data import *
+from .discord import *
+from .embeds import *
--- _typings/data/treasure.py
+++ _typings/data/treasure.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
-from typing import TypedDict
+from typing import List, TypedDict
 
-__all__ = ("TreasureData",)
+__all__ = ("TreasureData", "TreasureDataList", "RawTreasureData")
+
 
 class TreasureData(TypedDict):
     name: str
@@ -11,3 +12,10 @@ class TreasureData(TypedDict):
     bonus: int
     spawndate: str
     nation: str
+
+
+TreasureDataList = List[TreasureData]
+
+
+class RawTreasureData(TypedDict):
+    treasures: TreasureDataList
