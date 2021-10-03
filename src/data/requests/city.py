e4e717b6a0366a1c1bcbff6d0457165232d70bd7
mrvillage <mr.village05@gmail.com>
2021-10-03 16:38:39 -0400

    Remove data.requests

--- src/data/requests/city.py
+++
@@ -1,8 +0,0 @@
-import aiohttp
-
-
-async def get_city_build(*, city_id):
-    async with aiohttp.request(
-        "GET", f"https://politicsandwar.com/api/city_export.php?city_id={city_id}"
-    ) as response:
-        return await response.json()
