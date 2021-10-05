0f56d8e1b715c9683236300e1fc497a66d63424f
mrvillage <mr.village05@gmail.com>
2021-10-05 18:20:06 -0400

    Update parse_alliance_bank to remove await from convert_number

--- src/funcs/parse.py
+++ src/funcs/parse.py
@@ -14,9 +14,10 @@ async def parse_alliance_bank(content: str) -> Dict[str, Union[float, int]]:
     tables = soup.find_all(class_="nationtable")
     table = tables[0]
     contents = [i for i in table.contents if i != "\n"]
-    data = {}
-    for content in contents[1:]:
-        data[content.contents[0].contents[1].strip(" ").lower()] = await convert_number(  # type: ignore
-            content.contents[1].contents[0]  # type: ignore
-        )
-    return data  # type: ignore
+    return {
+        content.contents[0]
+        .contents[1]
+        .strip(" ")
+        .lower(): convert_number(content.contents[1].contents[0])
+        for content in contents[1:]
+    }
