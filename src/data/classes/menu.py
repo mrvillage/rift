450295bbb8daaaf0e9b210bdf34ae0aed8cedbb1
mrvillage <mr.village05@gmail.com>
2021-10-06 11:03:16 -0400

    Move menu view classes to views.Menus and change to use convert_int

--- src/data/classes/menu.py
+++ src/data/classes/menu.py
@@ -13,7 +13,9 @@ from ...errors import (
     MenuNotFoundError,
     TicketConfigNotFoundError,
 )
-from ...ref import Rift, RiftContext
+from ...funcs.utils import convert_int
+from ...ref import RiftContext
+from ...views.menu import MenuButton, MenuSelect, MenuSelectOption, MenuView
 from ..db import execute_query, execute_read_query
 from ..query import insert_interface
 from .base import Makeable
@@ -22,198 +24,6 @@ __all__ = ("Menu", "MenuItem")
 
 if TYPE_CHECKING:
     from _typings import MenuData, MenuFormattedFlags, MenuItemData
-
-    from .embassy import Embassy
-    from .ticket import Ticket
-
-
-class View(discord.ui.View):
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.menu_id: int = kwargs.pop("menu_id")
-        self.bot: Rift = kwargs.pop("bot")
-        super().__init__(*args, **kwargs)
-
-    async def interaction_check(self, interaction: discord.Interaction) -> bool:
-        if TYPE_CHECKING:
-            assert isinstance(interaction.message, discord.Message)
-        return bool(
-            [
-                i
-                for i in cache.menu_interfaces
-                if i["menu_id"] == self.menu_id
-                and i["message_id"] == interaction.message.id
-            ]
-        )
-
-
-class Button(discord.ui.Button):
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.action: str = kwargs.pop("action")
-        self.options: List[int] = kwargs.pop("options")
-        super().__init__(*args, **kwargs)
-
-    async def callback(self, interaction: discord.Interaction) -> None:
-        # sourcery no-metrics
-        from ...funcs import get_embed_author_member
-        from ..get import get_link_user
-        from .alliance import Alliance
-        from .embassy import EmbassyConfig
-        from .nation import Nation
-        from .ticket import TicketConfig
-
-        if TYPE_CHECKING:
-            assert isinstance(interaction.user, discord.Member)
-            assert isinstance(interaction.guild, discord.Guild)
-        bot_member = interaction.guild.get_member(interaction.application_id)
-        if TYPE_CHECKING:
-            assert isinstance(bot_member, discord.Member)
-        highest_role = bot_member.top_role
-        if self.action is None:
-            return await interaction.response.defer()
-        if self.action in {"ADD_ROLE", "ADD_ROLES"}:
-            roles: List[discord.Role] = []
-            for role in self.options:
-                role = interaction.guild.get_role(role)
-                if role is None:
-                    continue
-                if role < highest_role and role not in interaction.user.roles:
-                    roles.append(role)
-            if roles:
-                await interaction.user.add_roles(*roles)
-                await interaction.response.send_message(
-                    ephemeral=True,
-                    embed=get_embed_author_member(
-                        interaction.user,
-                        f"Added the following roles: {', '.join(role.mention for role in roles)}",
-                    ),
-                )
-            else:
-                await interaction.response.send_message(
-                    ephemeral=True,
-                    embed=get_embed_author_member(
-                        interaction.user,
-                        "No roles were added since you have them all already.",
-                    ),
-                )
-        elif self.action in {"REMOVE_ROLE", "REMOVE_ROLES"}:
-            roles = []
-            for role in self.options:
-                role = interaction.guild.get_role(role)
-                if role is None:
-                    continue
-                if role < highest_role and role not in interaction.user.roles:
-                    roles.append(role)
-            if roles:
-                await interaction.user.remove_roles(*roles)
-                await interaction.response.send_message(
-                    ephemeral=True,
-                    embed=get_embed_author_member(
-                        interaction.user,
-                        f"Removed the following roles from you: {', '.join(role.mention for role in roles)}",
-                    ),
-                )
-            else:
-                await interaction.response.send_message(
-                    ephemeral=True,
-                    embed=get_embed_author_member(
-                        interaction.user,
-                        "No roles were removed since you you don't have any of them.",
-                    ),
-                )
-        elif self.action in {"CREATE_TICKET", "CREATE_TICKETS"}:
-            await interaction.response.defer()
-            configs = [await TicketConfig.fetch(opt) for opt in self.options]
-            tickets: List[Ticket] = []
-            for config in configs:
-                if config.guild_id != interaction.guild.id:
-                    continue
-                ticket = await config.create(interaction.user)
-                await ticket.start(interaction.user, config)
-                tickets.append(ticket)
-            if len(tickets) > 1:
-                tickets_str = "\n".join(f"<#{ticket.id}" for ticket in tickets)
-                await interaction.followup.send(
-                    ephemeral=True,
-                    embed=get_embed_author_member(
-                        interaction.user, f"Tickets Created!\n{tickets_str}"
-                    ),
-                )
-            else:
-                await interaction.followup.send(
-                    ephemeral=True,
-                    embed=get_embed_author_member(
-                        interaction.user, f"Ticket Created!\n<#{tickets[0].id}>"
-                    ),
-                )
-        elif self.action in {"CLOSE_TICKET", "CREATE_TICKETS"}:
-            ...
-        elif self.action in {"CREATE_EMBASSY", "CREATE_EMBASSIES"}:
-            await interaction.response.defer()
-            configs = [await EmbassyConfig.fetch(opt) for opt in self.options]
-            embassies: List[Embassy] = []
-            try:
-                nation = await get_link_user(interaction.user.id)
-                nation = await Nation.fetch(nation["nation_id"])
-                alliance = await Alliance.fetch(nation.alliance_id)
-                if nation.alliance_position not in {"Officer", "Heir", "Leader"}:
-                    raise KeyError
-            except IndexError:
-                return await interaction.followup.send(
-                    ephemeral=True,
-                    embed=get_embed_author_member(
-                        interaction.user,
-                        "You must be linked to create an embassy.",
-                        color=discord.Color.red(),
-                    ),
-                )
-            except KeyError:
-                return await interaction.followup.send(
-                    ephemeral=True,
-                    embed=get_embed_author_member(
-                        interaction.user,
-                        "You must be an Officer or higher to create an embassy.",
-                        color=discord.Color.red(),
-                    ),
-                )
-            for config in configs:
-                if config.guild_id != interaction.guild.id:
-                    continue
-                embassy, start = await config.create(interaction.user, alliance)
-                if start:
-                    await embassy.start(interaction.user, config)
-                embassies.append(embassy)
-            if len(embassies) > 1:
-                embassies_str = "\n".join(f"<#{embassy.id}" for embassy in embassies)
-                await interaction.followup.send(
-                    ephemeral=True,
-                    embed=get_embed_author_member(
-                        interaction.user, f"Embassies:\n{embassies_str}"
-                    ),
-                )
-            else:
-                await interaction.followup.send(
-                    ephemeral=True,
-                    embed=get_embed_author_member(
-                        interaction.user, f"Embassy:\n<#{embassies[0].id}>"
-                    ),
-                )
-        elif self.action in {"CLOSE_EMBASSY", "CLOSE_EMBASSIES"}:
-            ...
-
-
-class Select(discord.ui.Select):
-    async def callback(self, interaction: discord.Interaction) -> None:
-        values = self.values.copy()  # type: ignore
-
-
-class SelectOption(discord.SelectOption):
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        self.action: str = kwargs.pop("action")
-        self.options: List[int] = kwargs.pop("options")
-        super().__init__(*args, **kwargs)
-
-    def __str__(self) -> str:
-        return f"Label: {self.label} - Description: {self.description} - Emoji: {self.emoji} - Default: {self.default} - Action: {self.action}"  # type: ignore
 
 
 class Menu(Makeable):
@@ -245,7 +55,7 @@ class Menu(Makeable):
         if TYPE_CHECKING:
             assert isinstance(ctx.guild, discord.Guild)
         try:
-            return await cls.fetch(int(argument), ctx.guild.id)
+            return await cls.fetch(convert_int(argument), ctx.guild.id)
         except ValueError:
             raise MenuNotFoundError(argument)
 
@@ -258,7 +68,7 @@ class Menu(Makeable):
 
     @classmethod
     def default(cls, guild_id: int) -> Menu:
-        menu = cls(
+        return cls(
             data={  # type: ignore
                 "guild_id": guild_id,
                 "name": None,
@@ -267,7 +77,6 @@ class Menu(Makeable):
                 "permissions": {},
             }
         )
-        return menu
 
     async def set_(self, **kwargs: Mapping[str, Any]) -> Menu:
         sets = [f"{key} = ${e+2}" for e, key in enumerate(kwargs)]
@@ -313,11 +122,11 @@ class Menu(Makeable):
                 if j.id == item_id:
                     del j
 
-    async def get_view(self) -> View:
+    async def get_view(self) -> MenuView:
         from ...ref import bot
 
         await self.make_attrs("items")
-        self.view = View(bot=bot, menu_id=self.id, timeout=None)
+        self.view = MenuView(bot=bot, menu_id=self.id, timeout=None)
         for index, item_set in enumerate(self.items):
             for item in item_set:
                 self.view.add_item(item.get_item(self.id, index))  # type: ignore
@@ -362,7 +171,7 @@ class MenuItem:
         if TYPE_CHECKING:
             assert isinstance(ctx.guild, discord.Guild)
         try:
-            return await cls.fetch(int(argument), ctx.guild.id)
+            return await cls.fetch(convert_int(argument), ctx.guild.id)
         except ValueError:
             raise MenuItemNotFoundError(argument)
 
@@ -373,10 +182,10 @@ class MenuItem:
             return item
         raise MenuItemNotFoundError(item_id)
 
-    def get_item(self, menu_id: int, row: int) -> Union[Button, Select]:
+    def get_item(self, menu_id: int, row: int) -> Union[MenuButton, MenuSelect]:
         custom_id = f"{menu_id}-{self.id}"
         if self.type == "button":
-            return Button(
+            return MenuButton(
                 style=discord.ButtonStyle[self.data.get("style", "blurple")],
                 label=l
                 if (l := self.data.get("label", None)) or self.data.get("emoji", None)
@@ -390,13 +199,13 @@ class MenuItem:
                 options=self.data.get("options", []),
             )
         elif self.type == "select":
-            return Select(
+            return MenuSelect(
                 custom_id=custom_id,
                 placeholder=self.data.get("placeholder", None),
                 min_values=self.data.get("min_values", 1),
                 max_values=self.data.get("max_values", 1),
                 options=[
-                    SelectOption(
+                    MenuSelectOption(
                         label=option["label"],
                         description=option.get("description", None),
                         emoji=option.get("emoji", None),
