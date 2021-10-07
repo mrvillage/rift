5f264ca15ee29520af44af7da9096c3524e2d50c
mrvillage <mr.village05@gmail.com>
2021-10-06 22:18:52 -0400

    Add `__all__` to owner.py

--- src/checks/owner.py
+++ src/checks/owner.py
@@ -4,9 +4,11 @@ from discord.ext.commands import check
 
 from ..ref import RiftContext
 
+__all__ = ("is_owner",)
+
 
 def is_owner():
     async def predicate(ctx: RiftContext):
-        return await ctx.bot.is_owner(ctx.author) # type: ignore
+        return await ctx.bot.is_owner(ctx.author)  # type: ignore
 
-    return check(predicate) # type: ignore
+    return check(predicate)  # type: ignore
