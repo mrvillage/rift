f176b0bc54f792fffe42b8a11023a37798c0fbaf
mrvillage <mr.village05@gmail.com>
2021-10-06 22:18:12 -0400

    Update manage checks for manager roles

--- src/checks/manage.py
+++ src/checks/manage.py
@@ -4,19 +4,31 @@ from typing import TYPE_CHECKING
 
 from discord.ext import commands
 
+from ..data.classes import GuildSettings
 from ..ref import RiftContext
 
 if TYPE_CHECKING:
     import discord
+__all__ = ("has_manage_permissions",)
 
 
-def has_manage_permissions():
+def has_manage_permissions(managers: bool = True):
     async def predicate(ctx: RiftContext):
         if TYPE_CHECKING:
             assert isinstance(ctx.author, discord.Member)
             assert isinstance(ctx.guild, discord.Guild)
         perms = ctx.author.guild_permissions
-        if ctx.bot.enable_debug and await ctx.bot.is_owner(ctx.author):  # type: ignore
+        settings = await GuildSettings.fetch(ctx.guild.id)
+        if (
+            ctx.bot.enable_debug
+            and await ctx.bot.is_owner(ctx.author)  # type: ignore
+            or (
+                managers
+                and any(
+                    i.id in (settings.manager_role_ids or []) for i in ctx.author.roles
+                )
+            )
+        ):
             return True
         if (
             perms.manage_guild
