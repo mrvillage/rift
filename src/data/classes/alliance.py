2b5ed95747ab377cb48c9f0e4c8730cb8b2b050a
mrvillage <mr.village05@gmail.com>
2021-10-06 21:40:32 -0400

    Fix upkeep_total key in alliance.calculate_revenue

--- src/data/classes/alliance.py
+++ src/data/classes/alliance.py
@@ -425,7 +425,7 @@ class Alliance(Makeable):
                 (i["net_total"] for i in revenues[1:]), revenues[0]["net_total"]
             ),
             "upkeep": sum((i["upkeep"] for i in revenues[1:]), revenues[0]["upkeep"]),
-            "gross_total": sum(
+            "upkeep_total": sum(
                 (i["upkeep_total"] for i in revenues[1:]), revenues[0]["upkeep_total"]
             ),
         }
