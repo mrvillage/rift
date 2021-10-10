7012e4b874ca58340c304b84f331a6024955c385
mrvillage <mr.village05@gmail.com>
2021-10-10 12:14:55 -0400

    Update connection to remove SSL certificate (does it itself now)

--- src/data/db/connect.py
+++ src/data/db/connect.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 import json
 from asyncio import get_event_loop
-from pathlib import Path
-from ssl import create_default_context
 
 from asyncpg import Connection, Pool, create_pool
 
@@ -18,14 +16,12 @@ async def init(conn: Connection) -> Connection:
 
 
 async def _create_connection() -> Pool:
-    path = Path(__file__).parent.parent.parent.parent / "db.crt"
     return await create_pool(  # type: ignore
         host=DB_HOST,
         port=DB_PORT,
         user=DB_USER,
         password=DB_PASSWORD,
         database=DB_NAME,
-        ssl=create_default_context(cafile=path),
         init=init,
     )
 
