2d53def4e6027ed74e03d8ddb7ad95b567765227
mrvillage <mr.village05@gmail.com>
2021-10-21 11:42:28 -0400

    Add status message when database cache filled

--- src/bot/cogs/database_cache.py
+++ src/bot/cogs/database_cache.py
@@ -15,6 +15,7 @@ class DatabaseCache(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         await funcs.fill_database_cache()
+        print("Database cache filled!")
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild: discord.Guild):
