0d564d654ded31b7be7ef553ce043c6fe99f72e6
mrvillage <mr.village05@gmail.com>
2021-11-21 22:31:43 -0500

    Update RolePermissions with Leadership permission

--- src/flags/permissions.py
+++ src/flags/permissions.py
@@ -69,69 +69,73 @@ class RolePermissions(Flags):
         return 1 << 0
 
     @flag
-    def view_alliance_bank(self):
+    def leadership(self):
         return 1 << 1
 
     @flag
-    def send_alliance_bank(self):
+    def view_alliance_bank(self):
         return 1 << 2
 
     @flag
-    def view_nation_banks(self):
+    def send_alliance_bank(self):
         return 1 << 3
 
     @flag
-    def view_nation_spies(self):
+    def view_nation_banks(self):
         return 1 << 4
 
     @flag
-    def view_offshores(self):
+    def view_nation_spies(self):
         return 1 << 5
 
     @flag
-    def send_offshore_banks(self):
+    def view_offshores(self):
         return 1 << 6
 
     @flag
-    def manage_offshores(self):
+    def send_offshore_banks(self):
         return 1 << 7
 
     @flag
-    def view_nation_safekeeping(self):
+    def manage_offshores(self):
         return 1 << 8
 
     @flag
-    def send_nation_safekeeping(self):
+    def view_nation_safekeeping(self):
         return 1 << 9
 
     @flag
-    def manage_nation_safekeeping(self):
+    def send_nation_safekeeping(self):
         return 1 << 10
 
     @flag
-    def approve_grants(self):
+    def manage_nation_safekeeping(self):
         return 1 << 11
 
     @flag
-    def manage_grants(self):
+    def approve_grants(self):
         return 1 << 12
 
     @flag
-    def manage_alliance_taxes(self):
+    def manage_grants(self):
         return 1 << 13
 
     @flag
-    def manage_alliance_positions(self):
+    def manage_alliance_taxes(self):
         return 1 << 14
 
     @flag
-    def manage_treaties(self):
+    def manage_alliance_positions(self):
         return 1 << 15
 
     @flag
-    def create_alliance_announcements(self):
+    def manage_treaties(self):
         return 1 << 16
 
     @flag
-    def delete_alliance_announcements(self):
+    def create_alliance_announcements(self):
         return 1 << 17
+
+    @flag
+    def delete_alliance_announcements(self):
+        return 1 << 18
