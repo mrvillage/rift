cc499f6d024c6472e40db32790b5b34d945b8ab0
mrvillage <mr.village05@gmail.com>
2021-11-22 22:33:20 -0500

    Add members to RoleData

--- _typings/data/role.py
+++ _typings/data/role.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Optional, TypedDict
+from typing import List, Optional, TypedDict
 
 __all__ = ("RoleData",)
 
@@ -12,3 +12,4 @@ class RoleData(TypedDict):
     alliance: int
     rank: int
     permissions: int
+    members: List[int]
