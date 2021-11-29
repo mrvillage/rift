d4d878053b8920c93d4de3827249e2a2b07bffb5
mrvillage <mr.village05@gmail.com>
2021-11-29 09:38:02 -0500

    Add Credentials.login and fix credentials properties to return None if None

--- src/data/classes/credentials.py
+++ src/data/classes/credentials.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Optional
 
+import aiohttp
+
 from ...cache import cache
 from ...flags import CredentialsPermissions
 from ...funcs import credentials
@@ -10,6 +12,8 @@ from .nation import Nation
 __all__ = ("Credentials",)
 
 if TYPE_CHECKING:
+    from typing import Optional
+
     from _typings import CredentialsData
 
 
@@ -26,15 +30,21 @@ class Credentials:
         self._api_key: str = data["api_key"]
 
     @property
-    def username(self) -> str:
+    def username(self) -> Optional[str]:
+        if self._username is None:
+            return
         return credentials.decrypt_credential(bytes.fromhex(self._username))
 
     @property
-    def password(self) -> str:
+    def password(self) -> Optional[str]:
+        if self._password is None:
+            return
         return credentials.decrypt_credential(bytes.fromhex(self._password))
 
     @property
-    def api_key(self) -> str:
+    def api_key(self) -> Optional[str]:
+        if self._api_key is None:
+            return
         return credentials.decrypt_credential(bytes.fromhex(self._api_key))
 
     @property
@@ -49,3 +59,15 @@ class Credentials:
         self._username: str = data["username"]
         self._password: str = data["password"]
         self._api_key: str = data["api_key"]
+
+    async def login(self, session: aiohttp.ClientSession) -> bool:
+        async with session.request(
+            "POST",
+            "https://politicsandwar.com/login/",
+            data={
+                "email": self.username,
+                "password": self.password,
+                "loginform": "Login",
+            },
+        ) as response:
+            return "login failure" not in (await response.text()).lower()
