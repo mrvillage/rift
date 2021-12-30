b129ad2f6f413623dc7ed774f975f4616e798902
mrvillage <mr.village05@gmail.com>
2021-12-30 00:31:01 -0500

    Delete from cache_roles and cache_channels when guild is removed

--- src/funcs/database_cache.py
+++ src/funcs/database_cache.py
@@ -90,6 +90,14 @@ async def fill_database_cache() -> None:  # sourcery no-metrics
                 "DELETE FROM cache_members WHERE guild = $1;",
                 guild["id"],
             )
+            await execute_query(
+                "DELETE FROM cache_roles WHERE guild = $1;",
+                guild["id"],
+            )
+            await execute_query(
+                "DELETE FROM cache_channels WHERE guild = $1;",
+                guild["id"],
+            )
             await execute_query("DELETE FROM cache_guilds WHERE id = $1;", guild["id"])
     for member in current_members:
         if (member["id"], member["guild"]) not in member_ids:
