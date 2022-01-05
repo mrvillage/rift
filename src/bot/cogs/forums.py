ecc9b7b02b7ec4db67cad09c47241ccca3ca8085
mrvillage <mr.village05@gmail.com>
2022-01-05 10:27:19 -0500

    Increase forum timeouts to five minutes

--- src/bot/cogs/forums.py
+++ src/bot/cogs/forums.py
@@ -25,7 +25,7 @@ class Forums(commands.Cog):
     @tasks.loop(minutes=1)
     async def check_alliance_affairs(self):
         try:
-            await asyncio.wait_for(self._check_alliance_affairs(), timeout=60)
+            await asyncio.wait_for(self._check_alliance_affairs(), timeout=300)
         except asyncio.TimeoutError:
             await self.send_loop_error("check_alliance_affairs")
 
@@ -68,7 +68,7 @@ class Forums(commands.Cog):
     @tasks.loop(minutes=1)
     async def check_orbis_central(self):
         try:
-            await asyncio.wait_for(self._check_orbis_central(), timeout=60)
+            await asyncio.wait_for(self._check_orbis_central(), timeout=300)
         except asyncio.TimeoutError:
             await self.send_loop_error("check_orbis_central")
 
