8dbf8b972b0ebc418f6a9a3f66085c67fec1b601
mrvillage <mr.village05@gmail.com>
2022-03-24 17:35:20 -0400

    Add resources type to schema

--- volumes/db/init/04-rift.sql
+++ volumes/db/init/04-rift.sql
@@ -1,3 +1,18 @@
+CREATE TYPE "resources" AS (
+  "money" numeric,
+  "coal" numeric,
+  "oil" numeric,
+  "uranium" numeric,
+  "iron" numeric,
+  "bauxite" numeric,
+  "lead" numeric,
+  "gasoline" numeric,
+  "munitions" numeric,
+  "steel" numeric,
+  "aluminum" numeric,
+  "food" numeric
+);
+
 CREATE TABLE "users" (
   "user_id" bigint,
   "nation_id" bigint,
