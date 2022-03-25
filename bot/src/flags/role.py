94bb1ea55049dd7cd85bc6f75fd71206a5b13def
mrvillage <mr.village05@gmail.com>
2022-03-25 12:08:45 -0400

    Scaffold role permission flags

---
+++ bot/src/flags/role.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from .common import CommonFlags
+
+__all__ = ("GuildRolePermissions", "RolePermissions")
+
+
+class GuildRolePermissions(CommonFlags):
+    ...
+
+
+class RolePermissions(CommonFlags):
+    ...
