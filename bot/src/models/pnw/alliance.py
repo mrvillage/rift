59771604340c35ac0a8718af2843efb013bf111b
mrvillage <mr.village05@gmail.com>
2022-03-26 17:51:09 -0400

    Default to 0 for attributes on models.Resources

--- bot/src/models/pnw/alliance.py
+++ bot/src/models/pnw/alliance.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import datetime
 from typing import TYPE_CHECKING
 
 import attrs
@@ -9,9 +10,10 @@ from ... import enums, utils
 __all__ = ("Alliance",)
 
 if TYPE_CHECKING:
-    import datetime
     import decimal
     from typing import ClassVar
+
+    from pnwkit.data import Alliance as PnWKitAlliance
 
     from ... import models
     from ...types.models.pnw.alliance import Alliance as AllianceData
@@ -39,3 +41,21 @@ class Alliance:
     @classmethod
     def from_dict(cls, data: AllianceData) -> Alliance:
         ...
+
+    @classmethod
+    def from_data(cls, data: PnWKitAlliance) -> Alliance:
+        if TYPE_CHECKING:
+            assert isinstance(data.score, decimal.Decimal)
+        return cls(
+            id=int(data.id),
+            name=data.name,
+            acronym=data.acronym,
+            score=data.score,
+            color=getattr(enums.Color, data.color.upper()),
+            date=datetime.datetime.fromisoformat(data.date),
+            accepts_members=data.acceptmem,
+            flag=data.flag,
+            forum_link=data.forumlink,
+            discord=data.irclink,
+            estimated_resources=models.Resources(),
+        )
