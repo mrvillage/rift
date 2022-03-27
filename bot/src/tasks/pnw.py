5ea854ea7dc8f5dd1d8950d6bc7848e4a2918434
mrvillage <mr.village05@gmail.com>
2022-03-27 14:08:54 -0400

    Add five minute interval for PnWDataTask

--- bot/src/tasks/pnw.py
+++ bot/src/tasks/pnw.py
@@ -9,12 +9,15 @@ __all__ = ("PnWDataTask",)
 
 
 class PnWDataTask(CommonTask):
+    def __init__(self) -> None:
+        super().__init__(interval=300)
+
     async def before_task(self) -> None:
         next_run = utils.utcnow()
         # set next run equal to the next multiple of four minutes
         next_run = next_run.replace(
-            minute=(next_run.minute // 4) * 4, second=0, microsecond=0
-        ) + datetime.timedelta(minutes=4)
+            minute=(next_run.minute // 5) * 5, second=0, microsecond=0
+        ) + datetime.timedelta(minutes=5)
         await utils.sleep_until(next_run.timestamp())
 
     async def task(self) -> None:
