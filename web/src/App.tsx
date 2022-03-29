e365d87e2d202f453c715cdf4b350cac6948f2d4
mrvillage <mr.village05@gmail.com>
2022-03-29 10:08:22 -0400

    Add mdx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -11,6 +11,7 @@ import { Shell } from "./components/Shell";
 import { AppContextProvider } from "./context/AppContext";
 import { useState } from "react";
 import { Icon } from "./utils";
+import { MDXProvider } from "@mdx-js/react";
 
 function App() {
   const navigate = useNavigate();
@@ -26,47 +27,49 @@ function App() {
   return (
     <div className="App">
       <AppContextProvider>
-        <ColorSchemeProvider
-          colorScheme={colorScheme}
-          toggleColorScheme={toggleColorScheme}
-        >
-          <MantineProvider
-            theme={{ colorScheme, spacing: { xxs: 5, xxxs: 2 } }}
+        <MDXProvider>
+          <ColorSchemeProvider
+            colorScheme={colorScheme}
+            toggleColorScheme={toggleColorScheme}
           >
-            <NotificationsProvider>
-              <SpotlightProvider
-                shortcut={["mod + K", "mod + P", "/"]}
-                actions={[
-                  {
-                    title: "Home",
-                    description: "Home page",
-                    onTrigger: () => navigate("/"),
-                    icon: <Icon.Home />,
-                  },
-                  {
-                    title: "Docs",
-                    description: "Documentation",
-                    onTrigger: () => console.log("/docs"),
-                    icon: <Icon.Book />,
-                  },
-                ]}
-                searchIcon={<Icon.Search size={18} />}
-                searchPlaceholder="Search..."
-                nothingFoundMessage="Nothing found..."
-                highlightQuery
-                transition={{
-                  in: { transform: "translateY(0)", opacity: 1 },
-                  out: { transform: "translateY(-20px)", opacity: 0 },
-                  transitionProperty: "transform, opacity",
-                }}
-              >
-                <Shell>
-                  <></>
-                </Shell>
-              </SpotlightProvider>
-            </NotificationsProvider>
-          </MantineProvider>
-        </ColorSchemeProvider>
+            <MantineProvider
+              theme={{ colorScheme, spacing: { xxs: 5, xxxs: 2 } }}
+            >
+              <NotificationsProvider>
+                <SpotlightProvider
+                  shortcut={["mod + K", "mod + P", "/"]}
+                  actions={[
+                    {
+                      title: "Home",
+                      description: "Home page",
+                      onTrigger: () => navigate("/"),
+                      icon: <Icon.Home />,
+                    },
+                    {
+                      title: "Docs",
+                      description: "Documentation",
+                      onTrigger: () => console.log("/docs"),
+                      icon: <Icon.Book />,
+                    },
+                  ]}
+                  searchIcon={<Icon.Search size={18} />}
+                  searchPlaceholder="Search..."
+                  nothingFoundMessage="Nothing found..."
+                  highlightQuery
+                  transition={{
+                    in: { transform: "translateY(0)", opacity: 1 },
+                    out: { transform: "translateY(-20px)", opacity: 0 },
+                    transitionProperty: "transform, opacity",
+                  }}
+                >
+                  <Shell>
+                    <></>
+                  </Shell>
+                </SpotlightProvider>
+              </NotificationsProvider>
+            </MantineProvider>
+          </ColorSchemeProvider>
+        </MDXProvider>
       </AppContextProvider>
     </div>
   );
