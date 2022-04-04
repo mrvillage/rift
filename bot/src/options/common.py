54724d14b72b32a374e32873aeba828f38f602ae
mrvillage <mr.village05@gmail.com>
2022-04-04 12:00:05 -0400

    Add `CommonOption.__call__`

--- bot/src/options/common.py
+++ bot/src/options/common.py
@@ -8,7 +8,7 @@ __all__ = ("CommonOption",)
 
 if TYPE_CHECKING:
     from enum import EnumMeta
-    from typing import Any, Callable, Coroutine, Sequence, Union
+    from typing import Any, Callable, Coroutine, Sequence, TypeVar, Union
 
     from quarrel.interactions.command import OPTS
 
@@ -23,6 +23,8 @@ if TYPE_CHECKING:
         Callable[["CommonSlashCommand[OPTS]"], Coroutine[Any, Any, Any]],
         Callable[["CommonSlashCommand[OPTS]"], Any],
     ]
+
+    OPT = TypeVar("OPT", bound="CommonOption")
 
 
 class CommonOption(quarrel.Option):
@@ -57,3 +59,31 @@ class CommonOption(quarrel.Option):
         self.min_value: quarrel.Missing[float] = min_value
         self.max_value: quarrel.Missing[float] = max_value
         self.autocomplete: quarrel.Missing[bool] = autocomplete
+
+    def __call__(
+        self: OPT,
+        type: quarrel.Missing[quarrel.ApplicationCommandOptionType] = quarrel.MISSING,
+        name: quarrel.Missing[str] = quarrel.MISSING,
+        description: quarrel.Missing[str] = quarrel.MISSING,
+        converter: quarrel.Missing[ConverterCCC[OPTS]] = quarrel.MISSING,
+        converters: quarrel.Missing[list[ConverterCCC[OPTS]]] = quarrel.MISSING,
+        default: OptionDefault[OPTS] = quarrel.MISSING,
+        choices: quarrel.Missing[EnumMeta] = quarrel.MISSING,
+        channel_types: quarrel.Missing[Sequence[quarrel.ChannelType]] = quarrel.MISSING,
+        min_value: quarrel.Missing[float] = quarrel.MISSING,
+        max_value: quarrel.Missing[float] = quarrel.MISSING,
+        autocomplete: quarrel.Missing[bool] = quarrel.MISSING,
+    ) -> OPT:
+        return self.__class__(
+            self.type if type is quarrel.MISSING else type,
+            self.name if name is quarrel.MISSING else name,
+            self.description if description is quarrel.MISSING else description,
+            converter,
+            self.converters if converters is quarrel.MISSING else converters,
+            self.default if default is quarrel.MISSING else default,
+            self.choices if choices is quarrel.MISSING else choices,
+            self.channel_types if channel_types is quarrel.MISSING else channel_types,
+            self.min_value if min_value is quarrel.MISSING else min_value,
+            self.max_value if max_value is quarrel.MISSING else max_value,
+            self.autocomplete if autocomplete is quarrel.MISSING else autocomplete,
+        )
