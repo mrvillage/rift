8eb26d63d463df3902a39abb3b7e56e6fb9f2045
mrvillage <mr.village05@gmail.com>
2022-04-05 12:11:14 -0400

    Add Nation/AllianceNotFoundErrors

--- bot/src/errors/notfound.py
+++ bot/src/errors/notfound.py
@@ -2,7 +2,12 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-__all__ = ("NotFoundError", "MenuItemNotFoundError")
+__all__ = (
+    "NotFoundError",
+    "AllianceNotFoundError",
+    "MenuItemNotFoundError",
+    "NationNotFoundError",
+)
 
 from .base import RiftError
 
@@ -11,11 +16,20 @@ if TYPE_CHECKING:
 
 
 class NotFoundError(RiftError):
-    model: ClassVar[str]
+    name: ClassVar[str]
 
-    def __init_subclass__(cls, model: str) -> None:
-        cls.model = model
+    def __init_subclass__(cls, name: str) -> None:
+        # sourcery skip: instance-method-first-arg-name
+        cls.name = name
 
 
-class MenuItemNotFoundError(NotFoundError, model="menu item"):
+class AllianceNotFoundError(NotFoundError, name="alliance"):
+    ...
+
+
+class MenuItemNotFoundError(NotFoundError, name="menu item"):
+    ...
+
+
+class NationNotFoundError(NotFoundError, name="nation"):
     ...
