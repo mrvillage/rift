666bdae9901cb8e61e7581e7973c6c036c1c9278
mrvillage <mr.village05@gmail.com>
2022-05-10 12:11:03 -0400

    Fix in common components as well

--- bot/src/components/common.py
+++ bot/src/components/common.py
@@ -4,23 +4,12 @@ from typing import TYPE_CHECKING
 
 import quarrel
 
-from .. import embeds, utils
+from .. import utils
 
 __all__ = ("CommonButton", "CommonSelectMenu", "CommonGrid")
 
 if TYPE_CHECKING:
     ...
-
-
-class CommonComponent:
-    async def on_error(
-        self, interaction: quarrel.Interaction, error: Exception
-    ) -> None:
-        if utils.handle_interaction_error(interaction, error):
-            await quarrel.Button.on_error(self, error)  # type: ignore
-            await interaction.respond_with_message(
-                embed=embeds.fatal_error(interaction.user)
-            )
 
 
 class CommonButton(quarrel.Button):
@@ -29,20 +18,16 @@ class CommonButton(quarrel.Button):
     ) -> None:
         if utils.handle_interaction_error(interaction, error):
             await quarrel.Button.on_error(self, error)  # type: ignore
-            await interaction.respond_with_message(
-                embed=embeds.fatal_error(interaction.user)
-            )
 
 
 class CommonSelectMenu(quarrel.SelectMenu):
-    async def on_error(self, interaction: quarrel.Interaction, values: tuple[str], error: Exception) -> None:
-        
+    async def on_error(
+        self, interaction: quarrel.Interaction, values: tuple[str], error: Exception
+    ) -> None:
+
         # original = error
         if utils.handle_interaction_error(interaction, error):
             await quarrel.Button.on_error(self, error)  # type: ignore
-            await interaction.respond_with_message(
-                embed=embeds.fatal_error(interaction.user)
-            )
 
 
 class CommonGrid(quarrel.Grid):
