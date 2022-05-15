ce065ea77b8a49283bfccc5bbf95778ececfde33
mrvillage <mr.village05@gmail.com>
2022-05-14 21:13:08 -0400

    Fix users iteration

--- bot/tests/suite/models/users.py
+++ bot/tests/suite/models/users.py
@@ -7,6 +7,6 @@ __all__ = ("DATA",)
 DATA: dict[int, models.User] = {
     3001: models.User(user_id=3001, nation_id=1001, uuid=None)
 }
-for i in DATA.values():
+for i in set(DATA.values()):
     if i.nation_id is not None:
         DATA[i.nation_id] = i
