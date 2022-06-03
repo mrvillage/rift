127f4682560d038ffa96d994ee705357bbfea137
mrvillage <mr.village05@gmail.com>
2022-06-02 22:59:33 -0400

    Add checks.button_guild_only

--- bot/src/checks/core.py
+++ bot/src/checks/core.py
@@ -6,7 +6,7 @@ import quarrel
 
 from .. import errors
 
-__all__ = ("guild_only", "modal_guild_only")
+__all__ = ("guild_only", "modal_guild_only", "button_guild_only")
 
 if TYPE_CHECKING:
     from typing import Any
@@ -26,6 +26,16 @@ async def modal_guild_only(
     interaction: quarrel.Interaction,
     groups: dict[str, str],
     values: Any,
+) -> bool:
+    if interaction.guild_id is quarrel.MISSING:
+        raise errors.GuildOnlyError()
+    return True
+
+
+async def button_guild_only(
+    modal: quarrel.Component,
+    interaction: quarrel.Interaction,
+    groups: dict[str, str],
 ) -> bool:
     if interaction.guild_id is quarrel.MISSING:
         raise errors.GuildOnlyError()
