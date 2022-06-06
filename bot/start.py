9dac35f75f27525accf1f51c2799e81268313fe5
mrvillage <mr.village05@gmail.com>
2022-06-05 22:34:05 -0400

    Don't start PnWDataTask, just in case it hurts the API

--- bot/start.py
+++ bot/start.py
@@ -2,10 +2,10 @@ from __future__ import annotations
 
 import asyncio
 
+from src import db  # , tasks
 from src import cache
 from src import commands as commands
 from src import components as components
-from src import db, tasks
 from src.bot import bot
 
 
@@ -15,7 +15,7 @@ async def main() -> None:
     await db.create_notify_connection()
     print("Initializing cache...", flush=True)
     await cache.initialize()
-    bot.running_tasks = [tasks.PnWDataTask().start()]
+    # bot.running_tasks = [tasks.PnWDataTask().start()]
     print("Connecting to Discord...", flush=True)
     await bot.run()
 
