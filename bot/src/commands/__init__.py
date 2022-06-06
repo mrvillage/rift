773e3994428175987291df2345473571a00c9f5e
mrvillage <mr.village05@gmail.com>
2022-06-05 22:51:08 -0400

    Recomment commands

--- bot/src/commands/__init__.py
+++ bot/src/commands/__init__.py
@@ -1,25 +1,29 @@
-from .alliance_settings import *
-from .audit import *
-from .auto_role import *
-from .bank import *
-from .blitz import *
-from .build import *
+# from .alliance_settings import *
+# from .audit import *
+# from .auto_role import *
+# from .bank import *
+# from .blitz import *
+# from .build import *
 from .condition import *
 from .core import *
-from .embassy import *
+
+# from .embassy import *
 from .fun import *
-from .grant import *
-from .interview import *
-from .mention import *
+
+# from .grant import *
+# from .interview import *
+# from .mention import *
 from .menu import *
 from .militarization import *
-from .reminder import *
-from .role import *
-from .roster import *
-from .server import *
-from .server_settings import *
-from .subscription import *
+
+# from .reminder import *
+# from .role import *
+# from .roster import *
+# from .server import *
+# from .server_settings import *
+# from .subscription import *
 from .tag import *
 from .target import *
-from .ticket import *
-from .war_room import *
+
+# from .ticket import *
+# from .war_room import *
